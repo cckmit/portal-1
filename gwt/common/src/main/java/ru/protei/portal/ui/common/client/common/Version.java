431ce11acd7dca3a937bb1497561372192429a78
ponomareva <devnull@localhost>
2016-09-29 09:51:53 +0300

    add Version class

---
+++ gwt/common/src/main/java/ru/protei/portal/ui/common/client/common/Version.java
@@ -0,0 +1,10 @@
+package ru.protei.portal.ui.common.client.common;
+
+/**
+ * 4.0.0.0 - начальная версия
+ */
+public class Version {
+    public static String getVersion() {
+        return "4.0.0.0";
+    }
+}
