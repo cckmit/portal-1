d4d4282e3bd9f8c423331d7270dd4462099e230d
frost <devnull@localhost>
2016-10-13 11:59:47 +0300

    fill dev_unit, change request to server

---
+++ db/patches/patch.0004.sprint3.sql
@@ -0,0 +1,6 @@
+insert into dev_unit (CREATED, UTYPE_ID, CREATOR_ID, UNIT_STATE, UNIT_NAME, old_id) values (current_timestamp, 2, 46, 1, 'Billing', 281);
+insert into dev_unit (CREATED, UTYPE_ID, CREATOR_ID, UNIT_STATE, UNIT_NAME, old_id) values (current_timestamp, 2, 46, 1, 'CallCenter', 184);
+insert into dev_unit (CREATED, UTYPE_ID, CREATOR_ID, UNIT_STATE, UNIT_NAME, old_id) values (current_timestamp, 2, 46, 1, 'PROTEI DPI', 240);
+insert into dev_unit (CREATED, UTYPE_ID, CREATOR_ID, UNIT_STATE, UNIT_NAME, old_id) values (current_timestamp, 2, 46, 1, 'TravelSIM', 159);
+insert into dev_unit (CREATED, UTYPE_ID, CREATOR_ID, UNIT_STATE, UNIT_NAME, old_id) values (current_timestamp, 2, 46, 2, 'Мобильный портал', 57);
+insert into dev_unit (CREATED, UTYPE_ID, CREATOR_ID, UNIT_STATE, UNIT_NAME, old_id) values (current_timestamp, 2, 46, 2, 'OMS', 142);
