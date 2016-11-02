0b91bc532ff983282cb320c1911536fbc583304c
michael <devnull@localhost>
2016-11-02 08:52:47 +0300

    add logger

--- gwt/contact/src/main/java/ru/protei/portal/ui/contact/server/ContactServiceImpl.java
+++ gwt/contact/src/main/java/ru/protei/portal/ui/contact/server/ContactServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.protei.portal.ui.contact.server;
 
-import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.protei.portal.api.struct.CoreResponse;
@@ -12,8 +13,6 @@ import ru.protei.portal.core.model.query.ContactQuery;
 import ru.protei.portal.ui.common.shared.exception.RequestFailedException;
 import ru.protei.portal.ui.contact.client.service.ContactService;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -26,7 +25,11 @@ public class ContactServiceImpl implements ContactService {
     public List<Person> getContacts(String searchPattern, Company company, int isFired, En_SortField sortField, Boolean sortDir) throws RequestFailedException {
         ContactQuery query = new ContactQuery(company.getId(), searchPattern, sortField, sortDir ? En_SortDir.ASC : En_SortDir.DESC);
 
+        log.debug("before get contact list");
+
         CoreResponse<List<Person>> response = contactService.contactList(query);
+
+        log.debug("after get contact list");
 
         if (response.isError()) {
             throw new RequestFailedException();
@@ -49,5 +52,7 @@ public class ContactServiceImpl implements ContactService {
 
     @Autowired
     ru.protei.portal.core.service.dict.ContactService contactService;
+
+    private static final Logger log = LoggerFactory.getLogger( "web" );
 
 }
