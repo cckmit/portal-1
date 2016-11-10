0de2b071bba8d44419e2f96240a532868140d3fa
shagaleev <shagaleev@shagaleev.protei>
2016-11-10 08:40:14 +0300

    Case query implemented in cotroller (part2)

--- gwt/issue/src/main/java/ru/protei/portal/ui/issue/client/activity/table/IssueTableActivity.java
+++ gwt/issue/src/main/java/ru/protei/portal/ui/issue/client/activity/table/IssueTableActivity.java
@@ -5,16 +5,18 @@ import com.google.inject.Inject;
 import ru.brainworm.factory.generator.activity.client.activity.Activity;
 import ru.brainworm.factory.generator.activity.client.annotations.Event;
 import ru.brainworm.factory.generator.injector.client.PostConstruct;
+import ru.protei.portal.core.model.dict.En_SortDir;
 import ru.protei.portal.core.model.ent.CaseObject;
-import ru.protei.portal.core.model.ent.Person;
-import ru.protei.portal.ui.common.client.events.*;
+import ru.protei.portal.core.model.query.CaseQuery;
+import ru.protei.portal.ui.common.client.events.AppEvents;
+import ru.protei.portal.ui.common.client.events.AuthEvents;
+import ru.protei.portal.ui.common.client.events.IssueEvents;
+import ru.protei.portal.ui.common.client.events.NotifyEvents;
 import ru.protei.portal.ui.common.client.lang.Lang;
-import ru.protei.portal.ui.common.client.service.PeriodicTaskService;
 import ru.protei.portal.ui.common.shared.model.RequestCallback;
 import ru.protei.portal.ui.issue.client.service.IssueServiceAsync;
 
 import java.util.List;
-import java.util.function.Consumer;
 
 /**
  * Активность таблицы обращений
@@ -70,9 +72,7 @@ public abstract class IssueTableActivity implements AbstractIssueTableActivity, 
     private void requestIssues() {
         view.clearRecords();
 
-        issueService.getIssues( view.searchPattern().getValue(), null,
-            view.showFired().getValue() ? null : view.showFired().getValue(),
-            view.sortField().getValue(), view.sortDir().getValue(), new RequestCallback< List< CaseObject > >() {
+        issueService.getIssues( getQuery(), new RequestCallback< List< CaseObject > >() {
                 @Override
                 public void onError( Throwable throwable ) {
                     fireEvent( new NotifyEvents.Show( lang.errGetList(), NotifyEvents.NotifyType.ERROR ) );
@@ -85,6 +85,10 @@ public abstract class IssueTableActivity implements AbstractIssueTableActivity, 
                     } );
                 }
             } );
+    }
+
+    private CaseQuery getQuery() {
+        return new CaseQuery( (Long)null, view.searchPattern().getValue(), view.sortField().getValue(), view.sortDir().getValue() ? En_SortDir.ASC : En_SortDir.DESC );
     }
 
     @Inject
