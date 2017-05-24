6e70ba6812047a3b25917b14b2bec0d81d5f3ec8
michael <devnull@localhost>
2017-05-24 17:07:14 +0300

    HPSM, add logging to sender

--- module/hpsm/src/main/java/ru/protei/portal/hpsm/utils/JavaMailSendChannel.java
+++ module/hpsm/src/main/java/ru/protei/portal/hpsm/utils/JavaMailSendChannel.java
@@ -1,14 +1,19 @@
 package ru.protei.portal.hpsm.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import ru.protei.portal.hpsm.api.MailSendChannel;
 
+import javax.mail.Address;
 import javax.mail.internet.MimeMessage;
 
 /**
  * Created by michael on 25.04.17.
  */
 public class JavaMailSendChannel implements MailSendChannel {
+
+    private static Logger logger = LoggerFactory.getLogger(JavaMailSendChannel.class);
 
     private JavaMailSender sender;
 
@@ -18,6 +23,22 @@ public class JavaMailSendChannel implements MailSendChannel {
 
     @Override
     public void send(MimeMessage msg) {
+
+        if (logger.isDebugEnabled()) {
+            try {
+                StringBuilder sb = new StringBuilder();
+                for (Address a : msg.getAllRecipients()) {
+                    sb.append(a.toString());
+                    sb.append(" ");
+                }
+
+                logger.debug("send mail message to {}, subject = {}", sb.toString(), msg.getSubject());
+            }
+            catch (Throwable e) {
+                logger.debug("error while make debug-message, mail is sending");
+            }
+        }
+
         this.sender.send(msg);
     }
 }
