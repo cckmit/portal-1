75d594f3c9d9e3b937a72ec6388f11f84161ff0a
turik <devnull@localhost>
2017-06-14 19:18:33 +0300

    En_UserRole deleted

--- gwt/common/src/main/java/ru/protei/portal/ui/common/client/common/PolicyUtils.java
+++ gwt/common/src/main/java/ru/protei/portal/ui/common/client/common/PolicyUtils.java
@@ -1,7 +1,6 @@
 package ru.protei.portal.ui.common.client.common;
 
 
-import ru.protei.portal.core.model.dict.En_UserRole;
 import ru.protei.portal.core.model.ent.UserRole;
 import ru.protei.portal.ui.common.shared.model.Profile;
 
@@ -13,34 +12,34 @@ import java.util.stream.Collectors;
 public class PolicyUtils {
 
     public static boolean isAllowedDashboardTab( Profile profile ) {
-        return !profile.getRoles().stream().map( UserRole::getId ).collect( Collectors.toList() ).contains( En_UserRole.DN_ADMIN.getId() );
+        return !profile.getRoles().stream().map( UserRole::getId ).collect( Collectors.toList() ).contains( 5L );
     }
 
     public static boolean isAllowedCompanyTab( Profile profile ) {
-        return !profile.getRoles().stream().map( UserRole::getId ).collect( Collectors.toList() ).contains( En_UserRole.DN_ADMIN.getId() );
+        return !profile.getRoles().stream().map( UserRole::getId ).collect( Collectors.toList() ).contains( 5L );
     }
 
     public static boolean isAllowedProductTab( Profile profile ) {
-        return !profile.getRoles().stream().map( UserRole::getId ).collect( Collectors.toList() ).contains( En_UserRole.DN_ADMIN.getId() );
+        return !profile.getRoles().stream().map( UserRole::getId ).collect( Collectors.toList() ).contains( 5L );
     }
 
     public static boolean isAllowedContactTab( Profile profile ) {
-        return !profile.getRoles().stream().map( UserRole::getId ).collect( Collectors.toList() ).contains( En_UserRole.DN_ADMIN.getId() );
+        return !profile.getRoles().stream().map( UserRole::getId ).collect( Collectors.toList() ).contains( 5L );
     }
 
     public static boolean isAllowedIssueTab( Profile profile ) {
-        return !profile.getRoles().stream().map( UserRole::getId ).collect( Collectors.toList() ).contains( En_UserRole.DN_ADMIN.getId() );
+        return !profile.getRoles().stream().map( UserRole::getId ).collect( Collectors.toList() ).contains( 5L );
     }
 
     public static boolean isAllowedEquipmentTab( Profile profile ) {
-        return profile.getRoles().stream().map( UserRole::getId ).collect( Collectors.toList() ).contains( En_UserRole.DN_ADMIN.getId() );
+        return profile.getRoles().stream().map( UserRole::getId ).collect( Collectors.toList() ).contains( 5L );
     }
 
     public static boolean isAllowedRegionTab( Profile profile ) {
-        return !profile.getRoles().stream().map( UserRole::getId ).collect( Collectors.toList() ).contains( En_UserRole.DN_ADMIN.getId() );
+        return !profile.getRoles().stream().map( UserRole::getId ).collect( Collectors.toList() ).contains( 5L );
     }
 
     public static boolean isAllowedProjectTab( Profile profile ) {
-        return !profile.getRoles().stream().map( UserRole::getId ).collect( Collectors.toList() ).contains( En_UserRole.DN_ADMIN.getId() );
+        return !profile.getRoles().stream().map( UserRole::getId ).collect( Collectors.toList() ).contains( 5L );
     }
 }
