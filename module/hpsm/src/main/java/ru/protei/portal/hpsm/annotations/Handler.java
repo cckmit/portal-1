09cea80e8ee04a04079ae197a60d44e2760f50ad
svitkov <devnull@localhost>
2018-01-29 16:05:46 +0300

    Removed useless annotation

--- module/hpsm/src/main/java/ru/protei/portal/hpsm/annotations/Handler.java
+++
@@ -1,9 +0,0 @@
-package ru.protei.portal.hpsm.annotations;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Handler {
-    CaseHandler[] value();
-}
