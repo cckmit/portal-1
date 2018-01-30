565ec80bbe10993fcddbb4577c69e88d11ad59db
svitkov <devnull@localhost>
2018-01-30 19:10:54 +0300

    Minor clean

--- module/hpsm/src/test/java/ru/protei/portal/test/hpsm/handlers/inbound/CaseHandlersTest.java
+++ module/hpsm/src/test/java/ru/protei/portal/test/hpsm/handlers/inbound/CaseHandlersTest.java
@@ -1,21 +1,18 @@
 package ru.protei.portal.test.hpsm.handlers.inbound;
 
-import org.junit.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.protei.portal.core.model.dao.CaseObjectDAO;
 import ru.protei.portal.core.model.dao.ExternalCaseAppDAO;
-import ru.protei.portal.core.model.ent.CaseObject;
-import ru.protei.portal.core.model.ent.ExternalCaseAppData;
 import ru.protei.portal.core.service.CaseControlService;
-import ru.protei.portal.hpsm.api.HpsmStatus;
-import ru.protei.portal.hpsm.logic.HpsmEvent;
 import ru.protei.portal.hpsm.logic.InboundMainMessageHandler;
 import ru.protei.portal.hpsm.utils.HpsmTestUtils;
 import ru.protei.portal.hpsm.utils.TestServiceInstance;
 import ru.protei.portal.test.hpsm.config.HpsmTestConfiguration;
-
-import javax.mail.internet.MimeMessage;
 
 public class CaseHandlersTest {
     public static final String HPSM_TEST_CASE_ID2 = "hpsm-update-test-1";
@@ -27,7 +24,7 @@ public class CaseHandlersTest {
     }
 
     @Test
-    public void test001 () throws Exception {
+    public void testForUpdateHandler () throws Exception {
 
         TestServiceInstance testServiceInstance = ctx.getBean(TestServiceInstance.class);
         InboundMainMessageHandler handler = ctx.getBean(InboundMainMessageHandler.class);
@@ -37,33 +34,6 @@ public class CaseHandlersTest {
         HpsmTestUtils testUtils = ctx.getBean(HpsmTestUtils.class);
 
         boolean result = handler.handle(testUtils.createNewRequest(HPSM_TEST_CASE_ID2), testServiceInstance);
-
-        Assert.assertTrue(result);
-
-        MimeMessage responseMail = testServiceInstance.getSentMessage();
-
-        Assert.assertNotNull(responseMail);
-
-        HpsmEvent responseEvent = testUtils.parseEvent(responseMail);
-
-        Assert.assertNotNull(responseEvent);
-
-        System.out.println(responseEvent.getMailBodyText());
-
-        Assert.assertEquals(HpsmStatus.REGISTERED, responseEvent.getSubject().getStatus());
-
-        Assert.assertNotNull(responseEvent.getHpsmMessage());
-
-        System.out.println(responseEvent.getSubject());
-
-        ExternalCaseAppData appData = externalCaseAppDAO.getByExternalAppId(HPSM_TEST_CASE_ID2);
-
-        Assert.assertNotNull(appData);
-
-        CaseObject resultCase = caseObjectDAO.get(appData.getId());
-
-        Assert.assertNotNull(resultCase.getInitiator());
-        Assert.assertNotNull(resultCase.getInitiatorCompany());
     }
 
 
