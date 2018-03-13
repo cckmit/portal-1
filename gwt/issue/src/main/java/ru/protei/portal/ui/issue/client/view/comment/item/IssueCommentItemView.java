ca0772f52d2e3eb9d97b128a7ac4f5efaeb46119
shagaleev <shagaleev@shagaleev.protei>
2018-03-13 10:37:08 +0300

    CRM-80, fixed incorrect xml tags in comments

--- gwt/issue/src/main/java/ru/protei/portal/ui/issue/client/view/comment/item/IssueCommentItemView.java
+++ gwt/issue/src/main/java/ru/protei/portal/ui/issue/client/view/comment/item/IssueCommentItemView.java
@@ -5,6 +5,8 @@ import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.dom.client.ImageElement;
 import com.google.gwt.dom.client.LIElement;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.safehtml.shared.SafeHtml;
+import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
@@ -59,7 +61,7 @@ public class IssueCommentItemView
             this.hideOptions();
             return;
         }
-        this.message.getElement().setInnerHTML( value );
+        this.message.getElement().setInnerSafeHtml( SafeHtmlUtils.fromString( value ) );
         this.messageBlock.removeClassName( "hide" );
     }
 
