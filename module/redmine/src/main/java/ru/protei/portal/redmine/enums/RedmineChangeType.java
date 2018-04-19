c685b889f2eea1d77a6a4fc901c4762e508860fa
svitkov <devnull@localhost>
2018-04-19 21:16:29 +0300

    Added updates from redmine parsing and eager event pushing

---
+++ module/redmine/src/main/java/ru/protei/portal/redmine/enums/RedmineChangeType.java
@@ -0,0 +1,25 @@
+package ru.protei.portal.redmine.enums;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum RedmineChangeType {
+    STATUS_CHANGE("status_id"),
+    PRIORITY_CHANGE("89"),
+    DESCRIPTION_CHANGE("description"),
+    SUBJECT_CHANGE("subject");
+
+    private final String name;
+
+    RedmineChangeType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Optional<RedmineChangeType> findByName(String name) {
+        return Arrays.stream(values()).filter(x -> x.name.equals(name)).findFirst();
+    }
+}
