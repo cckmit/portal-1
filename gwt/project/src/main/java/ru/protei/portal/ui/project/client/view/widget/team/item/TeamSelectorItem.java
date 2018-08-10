29d8163ea6ac7ec75b6fe3d8f04b5a7eac622a03
buchmastov <buchmastov@protei.ru>
2018-08-10 10:16:00 +0300

    Rotate back team selector's members widget styles

--- gwt/project/src/main/java/ru/protei/portal/ui/project/client/view/widget/team/item/TeamSelectorItem.java
+++ gwt/project/src/main/java/ru/protei/portal/ui/project/client/view/widget/team/item/TeamSelectorItem.java
@@ -24,8 +24,6 @@ public class TeamSelectorItem extends Composite implements AbstractTeamSelectorI
     @Inject
     public void init() {
         initWidget(ourUiBinder.createAndBindUi(this));
-        members.setAddName("fa-plus", "");
-        members.setClearName("fa-trash", "");
     }
 
     @Override
