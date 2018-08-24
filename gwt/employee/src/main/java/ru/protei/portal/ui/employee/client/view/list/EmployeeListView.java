dd9842a2862a9d9962dd1b4b5f8b6904401c0931
Eliseev Alexey <eliseev@protei.ru>
2018-08-24 22:05:23 +0300

    add list variant for employees card

--- gwt/employee/src/main/java/ru/protei/portal/ui/employee/client/view/list/EmployeeListView.java
+++ gwt/employee/src/main/java/ru/protei/portal/ui/employee/client/view/list/EmployeeListView.java
@@ -1,8 +1,11 @@
 package ru.protei.portal.ui.employee.client.view.list;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.HasWidgets;
@@ -32,6 +35,20 @@ public class EmployeeListView extends Composite implements AbstractEmployeeListV
     @Override
     public HasWidgets getFilterContainer () { return filterContainer; }
 
+    @UiHandler("tableMode")
+    public void tableModeClick(ClickEvent event) {
+        plateMode.setVisible(true);tableMode.setVisible(false);
+        childContainer.getElement().removeClassName("plate-list");
+        childContainer.getElement().addClassName("table-list");
+    }
+
+    @UiHandler("plateMode")
+    public void plateModeClick(ClickEvent event) {
+        plateMode.setVisible(false);tableMode.setVisible(true);
+        childContainer.getElement().removeClassName("table-list");
+        childContainer.getElement().addClassName("plate-list");
+    }
+
     @UiField
     PlateList childContainer;
 
@@ -41,6 +58,10 @@ public class EmployeeListView extends Composite implements AbstractEmployeeListV
     @Inject
     @UiField
     Lang lang;
+    @UiField
+    Anchor plateMode;
+    @UiField
+    Anchor tableMode;
 
     AbstractEmployeeListActivity activity;
 
