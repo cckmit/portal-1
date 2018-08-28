5e56566674ff6ab64129133bd6a54698f996075a
Eliseev Alexey <eliseev@protei.ru>
2018-08-28 13:24:32 +0300

    fix. collapse panel methods order.

--- gwt/common/src/main/java/ru/protei/portal/ui/common/client/widget/collapse/CollapsablePanel.java
+++ gwt/common/src/main/java/ru/protei/portal/ui/common/client/widget/collapse/CollapsablePanel.java
@@ -3,6 +3,7 @@ package ru.protei.portal.ui.common.client.widget.collapse;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
@@ -13,6 +14,21 @@ import com.google.gwt.user.client.ui.Widget;
 import java.util.Iterator;
 
 public class CollapsablePanel extends Composite implements HasWidgets {
+
+    public CollapsablePanel() {
+        initWidget(ourUiBinder.createAndBindUi(this));
+        legend.addDomHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                if (getElement().hasClassName("collapsed")) {
+                    getElement().removeClassName("collapsed");
+                } else {
+                    getElement().addClassName("collapsed");
+                }
+            }
+        }, ClickEvent.getType());
+    }
+
     @Override
     public void add(Widget w) {
         container.add(w);
@@ -37,31 +53,13 @@ public class CollapsablePanel extends Composite implements HasWidgets {
         this.legend.getElement().setInnerHTML(legend);
     }
 
-    interface CollapseContainerUiBinder extends UiBinder<HTMLPanel, CollapsablePanel> {
-
-    }
-
-    private static CollapseContainerUiBinder ourUiBinder = GWT.create(CollapseContainerUiBinder.class);
-
-    public CollapsablePanel() {
-        initWidget(ourUiBinder.createAndBindUi(this));
-        legend.addDomHandler(new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-                if (getElement().hasClassName("collapsed")) {
-                    getElement().removeClassName("collapsed");
-                } else {
-                    getElement().addClassName("collapsed");
-                }
-            }
-        }, ClickEvent.getType());
-    }
-
     @UiField
     HTMLPanel container;
     @UiField
     HTMLPanel legend;
 
+    interface CollapseContainerUiBinder extends UiBinder<HTMLPanel, CollapsablePanel> {}
 
+    private static CollapseContainerUiBinder ourUiBinder = GWT.create(CollapseContainerUiBinder.class);
 
 }
