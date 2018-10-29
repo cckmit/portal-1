2d4cae3aa1d45d58dacfe476eb6512f4ee188392
ponomareva <devnull@localhost>
2018-10-29 02:45:25 +0200

    issue report

--- gwt/issuereport/src/main/java/ru/protei/portal/ui/issuereport/client/view/table/IssueReportTableView.java
+++ gwt/issuereport/src/main/java/ru/protei/portal/ui/issuereport/client/view/table/IssueReportTableView.java
@@ -95,8 +95,6 @@ public class IssueReportTableView extends Composite implements AbstractIssueRepo
     @UiField
     InfiniteTableWidget<Report> table;
     @UiField
-    HTMLPanel tableContainer;
-    @UiField
     HTMLPanel pagerContainer;
 
     @Inject
