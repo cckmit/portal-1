23c749fde9b6b86522d3cf7cef12b9700fae4053
buchmastov <buchmastov@protei.ru>
2018-10-29 17:33:37 +0300

    Change version

--- gwt/common/src/main/java/ru/protei/portal/ui/common/client/common/Version.java
+++ gwt/common/src/main/java/ru/protei/portal/ui/common/client/common/Version.java
@@ -284,9 +284,10 @@ package ru.protei.portal.ui.common.client.common;
  * 4.0.80.3 - PORTAL-418 В новом crm2.protei.ru не во всех заявках отображается текст
  * 4.0.80.4 - PORTAL-419 Новый CRM: Некорректно работает прокрутка страницы (общий скролл).
  * 4.0.80.5 - PORTAL-439 Проверка и исправление интеграции с YouTrack после обновления
+ * 4.0.80.9 - PORTAL-427 Добавить возможность "удаления" проекта из портала
  */
 public class Version {
     public static String getVersion() {
-        return "4.0.80.5";
+        return "4.0.80.9";
     }
 }
