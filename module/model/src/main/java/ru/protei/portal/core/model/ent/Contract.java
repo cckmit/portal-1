1db7cf9ec9a3c40e55dad787f8ba5728c223c49c
ponomareva <devnull@localhost>
2019-02-11 00:59:09 +0300

    apply contract changes

--- module/model/src/main/java/ru/protei/portal/core/model/ent/Contract.java
+++ module/model/src/main/java/ru/protei/portal/core/model/ent/Contract.java
@@ -1,5 +1,7 @@
 package ru.protei.portal.core.model.ent;
 
+import ru.protei.portal.core.model.dict.En_CaseState;
+import ru.protei.portal.core.model.dict.En_ContractState;
 import ru.protei.portal.core.model.struct.AuditableObject;
 import ru.protei.winter.jdbc.annotations.*;
 
@@ -27,6 +29,47 @@ public class Contract extends AuditableObject implements Serializable {
     @JdbcJoinedColumn(localColumn = "id", table = "case_object", remoteColumn = "id", mappedColumn = "CREATED", sqlTableAlias = "CO")
     private Date created;
 
+    /**
+     * Дата изменения
+     */
+    @JdbcJoinedColumn(localColumn = "id", table = "case_object", remoteColumn = "id", mappedColumn = "MODIFIED", sqlTableAlias = "CO")
+    private Date modified;
+
+    /**
+     * Менеджер
+     */
+    @JdbcJoinedColumn(localColumn = "id", table = "case_object", remoteColumn = "id", mappedColumn = "MANAGER", sqlTableAlias = "CO")
+    private Long managerId;
+
+    /**
+     * Куратор договора
+     */
+    @JdbcJoinedColumn(localColumn = "id", table = "case_object", remoteColumn = "id", mappedColumn = "INITIATOR", sqlTableAlias = "CO")
+    private Long curatorId;
+
+    /**
+     * Направление
+     */
+    @JdbcJoinedColumn(localColumn = "id", table = "case_object", remoteColumn = "id", mappedColumn = "product_id", sqlTableAlias = "CO")
+    private Long directionId;
+
+    /**
+     * Сумма
+     */
+    private Long cost;
+
+    /**
+     * Текущее состояние договора
+     */
+    @JdbcJoinedColumn(localColumn = "id", table = "case_object", remoteColumn = "id", mappedColumn = "STATE", sqlTableAlias = "CO")
+    private Integer stateId;
+
+    /**
+     * Номер
+     */
+    @JdbcJoinedColumn(localColumn = "id", table = "case_object", remoteColumn = "id", mappedColumn = "CASENO", sqlTableAlias = "CO")
+    private String number;
+
     @Override
     public String getAuditType() {
         return "Contract";
@@ -40,4 +83,22 @@ public class Contract extends AuditableObject implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public En_ContractState getState () {
+        return En_ContractState.getById(this.stateId);
+    }
+
+    public void setState (En_ContractState state) {
+        this.stateId = state.getId();
+    }
+
 }
+
+//        - дата подписания договора
+//        - срок действия договора
+//        - тип договора
+//        - номер (case_object#caseno)
+//        - Контрагент (селектор на компании)
+//        - предмет договора  (case_object#des)
+//        - сроки поставки и оплаты (тут срок может быть не один)
+//        - куратор договора (селектор на сотрудников отдела договоров) - пока ограничений по должностям нет
