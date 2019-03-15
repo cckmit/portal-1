d9a438fc6fe53bbdb5e6b99dc8008376563ddcaf
buchmastov <buchmastov@protei.ru>
2019-03-15 16:48:16 +0300

    Project model change event

--- gwt/project/src/main/java/ru/protei/portal/ui/project/client/activity/preview/ProjectPreviewActivity.java
+++ gwt/project/src/main/java/ru/protei/portal/ui/project/client/activity/preview/ProjectPreviewActivity.java
@@ -74,6 +74,7 @@ public abstract class ProjectPreviewActivity implements AbstractProjectPreviewAc
                 .withSuccess(result -> {
                     fireEvent(new ProjectEvents.Show());
                     fireEvent(new NotifyEvents.Show(lang.projectRemoveSucceeded(), NotifyEvents.NotifyType.SUCCESS));
+                    fireEvent(new ProjectEvents.ChangeModel());
                 })
         );
     }
@@ -107,6 +108,7 @@ public abstract class ProjectPreviewActivity implements AbstractProjectPreviewAc
             @Override
             public void onSuccess( Void aVoid ) {
                 fireEvent( new ProjectEvents.Changed( project ) );
+                fireEvent( new ProjectEvents.ChangeModel() );
             }
         });
     }
