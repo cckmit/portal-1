2ba979fb40d53a26fbdf1a258ef5f132a36e9d85
buchmastov <buchmastov@protei.ru>
2019-03-27 15:36:32 +0300

    Table pager with page numbers

--- gwt/issue/src/main/java/ru/protei/portal/ui/issue/client/activity/table/IssueTableActivity.java
+++ gwt/issue/src/main/java/ru/protei/portal/ui/issue/client/activity/table/IssueTableActivity.java
@@ -317,18 +317,13 @@ public abstract class IssueTableActivity
     }
 
     @Override
-    public void onPageChanged( int page ) {
-        pagerView.setCurrentPage( page+1 );
+    public void onPageChanged(int page) {
+        pagerView.setCurrentPage(page);
     }
 
     @Override
-    public void onFirstClicked() {
-        view.scrollTo( 0 );
-    }
-
-    @Override
-    public void onLastClicked() {
-        view.scrollTo( view.getPageCount()-1 );
+    public void onPageSelected(int page) {
+        view.scrollTo(page);
     }
 
     @Override
