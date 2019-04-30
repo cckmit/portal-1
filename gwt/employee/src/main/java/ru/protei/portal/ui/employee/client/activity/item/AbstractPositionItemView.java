89482ade34a098d48eb6a69e33f1cdd96592cff1
ponomareva <devnull@localhost>
2019-04-30 07:07:25 +0300

    add hidden fields to company

--- gwt/employee/src/main/java/ru/protei/portal/ui/employee/client/activity/item/AbstractPositionItemView.java
+++ gwt/employee/src/main/java/ru/protei/portal/ui/employee/client/activity/item/AbstractPositionItemView.java
@@ -7,7 +7,6 @@ import com.google.gwt.user.client.ui.IsWidget;
  */
 public interface AbstractPositionItemView extends IsWidget {
     void setActivity( AbstractPositionItemActivity activity );
-    void setCompany( String value );
     void setDepartment( String value );
     void setPosition( String value );
     void showMainInfo( boolean isMain );
