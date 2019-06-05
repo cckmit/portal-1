1b04fbdd490a4bc44b4d9b1f62820e8ba62d58b8
porubov <devnull@localhost>
2019-06-05 12:13:12 +0300

    add tags selector to report view

--- gwt/common/src/main/java/ru/protei/portal/ui/common/client/widget/selector/casetag/CaseTagModel.java
+++ gwt/common/src/main/java/ru/protei/portal/ui/common/client/widget/selector/casetag/CaseTagModel.java
@@ -20,7 +20,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public abstract class CaseTagModel implements Activity, SelectorModel<EntityOption> {
 
