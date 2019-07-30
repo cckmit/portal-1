ce1b7c67400c39e497a4059082805267853dd2e3
vologzhanin <devnull@localhost>
2019-07-30 13:27:57 +0300

    added test testChangeProductState

--- module/core/src/test/java/ru/protei/portal/test/service/ProductServiceTest.java
+++ module/core/src/test/java/ru/protei/portal/test/service/ProductServiceTest.java
@@ -21,6 +21,7 @@ import ru.protei.winter.core.CoreConfigurationContext;
 import ru.protei.winter.core.utils.beans.SearchResult;
 import ru.protei.winter.jdbc.JdbcConfigurationContext;
 
+import javax.validation.constraints.AssertTrue;
 import java.util.Date;
 
 /**
@@ -91,6 +92,34 @@ public class ProductServiceTest {
 
         log.info(" product name " + name + " is uniq = " + result.getData());
 
+    }
+
+    @Test
+    public void testChangeProductState(){
+        DevUnit product = new DevUnit();
+
+        product.setName("Test Product");
+        product.setCreated(new Date());
+        product.setCreatorId(1L);
+        product.setInfo("Unit-test");
+        product.setStateId(En_DevUnitState.ACTIVE.getId());
+        product.setTypeId(En_DevUnitType.PRODUCT.getId());
+
+        Assert.assertNotNull(devUnitDAO.persist(product));
+
+        product.setStateId(En_DevUnitState.DEPRECATED.getId());
+        CoreResponse toDeprecated = productService.changeProductState(null, product);
+        DevUnit productDeprecated =  devUnitDAO.get(1L);
+
+        product.setStateId(En_DevUnitState.ACTIVE.getId());
+        CoreResponse toActive = productService.changeProductState(null, product);
+        DevUnit productActive =  devUnitDAO.get(1L);
+
+        Assert.assertNotNull(toDeprecated);
+        Assert.assertEquals(En_DevUnitState.DEPRECATED, productDeprecated.getState());
+
+        Assert.assertNotNull(toActive);
+        Assert.assertEquals(En_DevUnitState.ACTIVE, productActive.getState());
     }
 
     @Autowired
