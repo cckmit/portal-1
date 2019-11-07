3401426ab313cbf7dcdefe2160d59668905d9587
buchmastov <buchmastov@protei.ru>
2019-11-07 12:42:43 +0300

    Fix decimal number with document type short name

--- gwt/equipment/src/main/java/ru/protei/portal/ui/equipment/client/activity/document/edit/EquipmentDocumentEditActivity.java
+++ gwt/equipment/src/main/java/ru/protei/portal/ui/equipment/client/activity/document/edit/EquipmentDocumentEditActivity.java
@@ -198,14 +198,12 @@ public abstract class EquipmentDocumentEditActivity implements Activity, Abstrac
     }
 
     private void fillDTO(Document document) {
-        boolean isNew = document.getId() == null;
-
         document.setName(view.name().getValue());
         document.setApproved(view.approved().getValue());
         document.setType(view.documentType().getValue());
         document.setVersion(view.version().getValue());
         document.setInventoryNumber(view.approved().getValue() ? view.inventoryNumber().getValue() : null);
-        document.setDecimalNumber(isNew ? view.decimalNumber().getValue() + "-" + view.documentType().getValue().getShortName() : view.decimalNumber().getValue());
+        document.setDecimalNumber(getDecimalNumberFromView(document));
         document.setContractor(Person.fromPersonShortView(view.contractor().getValue()));
         document.setRegistrar(Person.fromPersonShortView(view.registrar().getValue()));
         document.setAnnotation(view.annotation().getValue());
@@ -274,6 +272,17 @@ public abstract class EquipmentDocumentEditActivity implements Activity, Abstrac
             return lang.negativeInventoryNumber();
         }
         return null;
+    }
+
+    private String getDecimalNumberFromView(Document document) {
+        boolean isNew = document.getId() == null;
+        if (!isNew) {
+            return view.decimalNumber().getValue();
+        }
+        if (view.documentType().getValue() == null || StringUtils.isBlank(view.documentType().getValue().getShortName())) {
+            return view.decimalNumber().getValue();
+        }
+        return view.decimalNumber().getValue() + view.documentType().getValue().getShortName();
     }
 
     @Inject
