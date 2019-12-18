8f7943fa607d34807b73ca6c14ad12c36130f938
buchmastov <buchmastov@protei.ru>
2019-12-18 11:37:10 +0300

    4.1.26.16 - fix DocumentServiceImpl#createDocument

--- module/core/src/main/java/ru/protei/portal/core/service/DocumentServiceImpl.java
+++ module/core/src/main/java/ru/protei/portal/core/service/DocumentServiceImpl.java
@@ -141,7 +141,7 @@ public class DocumentServiceImpl implements DocumentService {
             return error(validationStatus);
         }
 
-        if (document.getApproved() && !withDoc) {
+        if (document.getApproved() && !withPdf) {
             return error(En_ResultStatus.INCORRECT_PARAMS);
         }
 
