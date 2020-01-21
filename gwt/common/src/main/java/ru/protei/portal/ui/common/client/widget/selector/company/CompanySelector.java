60ddca30ac436f5a3e8cc57c9662366fc40f8012
Evgeny Fomin <special_for_web@mail.ru>
2020-01-21 13:08:24 +0300

    fix text-center in company selector

--- gwt/common/src/main/java/ru/protei/portal/ui/common/client/widget/selector/company/CompanySelector.java
+++ gwt/common/src/main/java/ru/protei/portal/ui/common/client/widget/selector/company/CompanySelector.java
@@ -2,6 +2,8 @@ package ru.protei.portal.ui.common.client.widget.selector.company;
 
 import com.google.inject.Inject;
 import ru.protei.portal.core.model.view.EntityOption;
+import ru.protei.portal.ui.common.client.selector.SelectorItem;
+import ru.protei.portal.ui.common.client.selector.popup.item.PopupSelectorItem;
 import ru.protei.portal.ui.common.client.widget.selector.button.ButtonPopupSingleSelector;
 
 /**
@@ -12,5 +14,12 @@ public class CompanySelector extends ButtonPopupSingleSelector<EntityOption> {
     public void init(CompanyModel companyModel) {
         setAsyncModel(companyModel);
         setItemRenderer(value -> value == null ? defaultValue : value.getDisplayText());
+    }
+
+    @Override
+    protected SelectorItem makeSelectorItem(EntityOption value, String elementHtml) {
+        PopupSelectorItem item = new PopupSelectorItem();
+        item.setName(elementHtml);
+        return item;
     }
 }
