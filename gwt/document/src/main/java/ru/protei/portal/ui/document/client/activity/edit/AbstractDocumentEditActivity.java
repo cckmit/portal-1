a9aad014782811db9326a0b801a1d6c3ca72b571
vologzhanin <devnull@localhost>
2020-02-14 13:48:28 +0300

    967 remove docEdit and equipDocEdit. Rename docForm to docEdit

--- gwt/document/src/main/java/ru/protei/portal/ui/document/client/activity/edit/AbstractDocumentEditActivity.java
+++ gwt/document/src/main/java/ru/protei/portal/ui/document/client/activity/edit/AbstractDocumentEditActivity.java
@@ -2,7 +2,20 @@ package ru.protei.portal.ui.document.client.activity.edit;
 
 public interface AbstractDocumentEditActivity {
 
+    void onEquipmentChanged();
+
+    void onDocumentCategoryChanged();
+
+    void onProjectChanged();
+
+    void onDownloadPdf();
+
+    void onDownloadDoc();
+
+    void onApprovedChanged();
+
+    void onCancelClicked();
+
     void onSaveClicked();
 
-    void onCloseClicked();
 }
