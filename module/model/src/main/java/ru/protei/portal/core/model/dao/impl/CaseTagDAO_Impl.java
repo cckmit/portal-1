1d5ca4fbed019b9f96efb722be22ebd9d9680300
Eliseev Alexey <eliseev@protei.ru>
2020-02-21 20:17:02 +0300

    close

--- module/model/src/main/java/ru/protei/portal/core/model/dao/impl/CaseTagDAO_Impl.java
+++ module/model/src/main/java/ru/protei/portal/core/model/dao/impl/CaseTagDAO_Impl.java
@@ -52,38 +52,5 @@ public class CaseTagDAO_Impl extends PortalBaseJdbcDAO<CaseTag> implements CaseT
         }
 
         return new SqlCondition( condition.getSqlCondition(), condition.getSqlParameters() );
-/*
-        return new SqlCondition().build(((condition, args) -> {
-            condition.append("1=1");
-
-            if (StringUtils.isNotBlank(query.getName())) {
-                condition.append(" and case_tag.name like ?");
-                args.add(HelperFunc.makeLikeArg(query.getName(), true));
-            }
-
-            if (query.getCaseType() != null) {
-                condition.append(" and case_tag.case_type = ?");
-                args.add(query.getCaseType().getId());
-            }
-
-            if (query.getCompanyId() != null) {
-                condition.append(" and case_tag.company_id = ?");
-                args.add(query.getCompanyId());
-            }
-
-            if (CollectionUtils.isNotEmpty(query.getIds())) {
-                condition.append(" and case_tag.id in ");
-                args.add(HelperFunc.makeInArg(query.getIds()));
-            }
-
-            if (query.getCaseId() != null ) {
-                condition.append(" and case_tag.id in (select case_object_tag.tag_id " +
-                                                      "from case_object_tag " +
-                                                      "where case_object_tag.case_id = ?)");
-                args.add(query.getCaseId());
-            }
-        }));
-        */
-
     }
 }
