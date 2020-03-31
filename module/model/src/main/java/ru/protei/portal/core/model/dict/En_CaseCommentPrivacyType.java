79187eb06c49659fb37a9fefba7955b9e4286967
turik <devnull@localhost>
2020-03-31 19:34:17 +0300

    some changes

--- module/model/src/main/java/ru/protei/portal/core/model/dict/En_CaseCommentPrivacyType.java
+++ module/model/src/main/java/ru/protei/portal/core/model/dict/En_CaseCommentPrivacyType.java
@@ -1,16 +1,7 @@
 package ru.protei.portal.core.model.dict;
 
 public enum En_CaseCommentPrivacyType {
-    PUBLIC(0),
-    PRIVATE_CUSTOMERS(1),
-    PRIVATE(2);
-
-    private int id;
-    En_CaseCommentPrivacyType(int id) {
-        this.id = id;
-    }
-
-    public int getId() {
-        return id;
-    }
+    PUBLIC,
+    PRIVATE_CUSTOMERS,
+    PRIVATE
 }
