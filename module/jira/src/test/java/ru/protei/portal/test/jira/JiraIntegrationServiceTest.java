6509cd8f69af2c263ee0d6c460ea4b743e1066a7
porubov <devnull@localhost>
2020-04-02 10:17:19 +0300

    add jira privacy type test

--- module/jira/src/test/java/ru/protei/portal/test/jira/JiraIntegrationServiceTest.java
+++ module/jira/src/test/java/ru/protei/portal/test/jira/JiraIntegrationServiceTest.java
@@ -15,10 +15,12 @@ import ru.protei.portal.config.PortalConfig;
 import ru.protei.portal.core.event.AssembledCaseEvent;
 import ru.protei.portal.core.mail.MailSendChannel;
 import ru.protei.portal.core.model.dao.*;
+import ru.protei.portal.core.model.dict.En_CaseCommentPrivacyType;
 import ru.protei.portal.core.model.dict.En_CaseState;
 import ru.protei.portal.core.model.dict.En_CompanyCategory;
 import ru.protei.portal.core.model.dict.En_Gender;
 import ru.protei.portal.core.model.ent.*;
+import ru.protei.portal.core.model.query.CaseCommentQuery;
 import ru.protei.portal.core.service.AssemblerService;
 import ru.protei.portal.jira.service.JiraIntegrationService;
 import ru.protei.portal.jira.dto.JiraHookEventData;
@@ -34,9 +36,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -66,6 +66,9 @@ public class JiraIntegrationServiceTest {
     CaseObjectDAO caseObjectDAO;
 
     @Autowired
+    CaseCommentDAO caseCommentDAO;
+
+    @Autowired
     AssemblerService assemblerService;
     @Autowired
     MailSendChannel sendChannel;
@@ -77,12 +80,14 @@ public class JiraIntegrationServiceTest {
     private final String FILE_PATH_EMPTY_PROJECT_JSON = "issue.empty.project.json";
     private final String FILE_PATH_EMPTY_STATUS_JSON = "issue.empty.status.json";
     private final String FILE_PATH_COMPANY_GROUP_JSON = "issue.companygroup.json";
+    private final String FILE_PATH_PRIVACY_TYPE_JSON = "issue.privacy.type.json";
 
     private String jsonString;
     private String updatedJsonString;
     private String emptyKeyJsonString;
     private String unknownStatusJsonString;
     private String companyGroupJsonString;
+    private String privacyTypeJsonString;
 
     private static final Logger log = LoggerFactory.getLogger(JiraIntegrationServiceTest.class);
 
@@ -99,6 +104,8 @@ public class JiraIntegrationServiceTest {
         unknownStatusJsonString = new String(encoded, StandardCharsets.UTF_8);
         encoded = Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource(FILE_PATH_COMPANY_GROUP_JSON).getFile()));
         companyGroupJsonString = new String(encoded, StandardCharsets.UTF_8);
+        encoded = Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource(FILE_PATH_PRIVACY_TYPE_JSON).getFile()));
+        privacyTypeJsonString = new String(encoded, StandardCharsets.UTF_8);
     }
 
     @Test
@@ -135,6 +142,39 @@ public class JiraIntegrationServiceTest {
         caseEvent = jiraIntegrationService.updateOrCreate(endpoint, new JiraHookEventData(JiraHookEventType.ISSUE_UPDATED, issue));
         CaseObject object = caseObjectDAO.get(caseEvent.get().getCaseObjectId());
         Assert.assertEquals("Issue not updated", object.getState(), En_CaseState.OPENED);
+    }
+
+    @Test
+    public void createWithPrivacyType() throws Exception{
+        Issue issue = makeIssue(privacyTypeJsonString);
+        Assert.assertNotNull("Error parsing json for create", issue);
+
+        Company company = makeCompany();
+        Person person = makePerson(company);
+
+        JiraEndpoint endpoint = jiraEndpointDAO.getByProjectId(company.getId(), issue.getProject().getId());
+        endpoint.setPersonId(person.getId());
+
+        CompletableFuture<AssembledCaseEvent> caseEvent = jiraIntegrationService.create(endpoint, new JiraHookEventData(JiraHookEventType.ISSUE_CREATED, issue));
+        Assert.assertNotNull("Issue not created", caseEvent.get().getCaseObject().getId());
+
+        CaseObject object = caseObjectDAO.get(caseEvent.get().getCaseObjectId());
+        CaseCommentQuery query = new CaseCommentQuery();
+        query.setCaseObjectIds(Collections.singletonList(object.getId()));
+        List<CaseComment> comments = caseCommentDAO.getCaseComments(query);
+
+        Assert.assertTrue("no pubic comment create", comments.stream()
+                        .anyMatch(comment -> "NO ROLE".equals(comment.getText())
+                                && En_CaseCommentPrivacyType.PUBLIC == comment.getPrivacyType()));
+        Assert.assertTrue("no Project Customer Role comment create", comments.stream()
+                .anyMatch(comment -> "Project Customer Role".equals(comment.getText())
+                        && En_CaseCommentPrivacyType.PUBLIC == comment.getPrivacyType()));
+        Assert.assertTrue("no Project Administrator Role comment create", comments.stream()
+                .anyMatch(comment -> "Project Administrator Role".equals(comment.getText())
+                        && En_CaseCommentPrivacyType.PRIVATE_CUSTOMERS == comment.getPrivacyType()));
+        Assert.assertTrue("no Project Developer Role comment create", comments.stream()
+                .anyMatch(comment -> "Project Developer Role".equals(comment.getText())
+                        && En_CaseCommentPrivacyType.PRIVATE_CUSTOMERS == comment.getPrivacyType()));
     }
 
     @Test
