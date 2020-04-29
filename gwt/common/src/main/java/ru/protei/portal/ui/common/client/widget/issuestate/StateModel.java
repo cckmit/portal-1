857fac64c77d91abba25f7df11a72642231021a4
turik <devnull@localhost>
2020-04-30 00:23:08 +0300

    rollback fetchNextCaseStatesForWorkflow

--- gwt/common/src/main/java/ru/protei/portal/ui/common/client/widget/issuestate/StateModel.java
+++ gwt/common/src/main/java/ru/protei/portal/ui/common/client/widget/issuestate/StateModel.java
@@ -8,6 +8,7 @@ import ru.protei.portal.core.model.dict.En_CaseStateWorkflow;
 import ru.protei.portal.core.model.ent.CaseState;
 import ru.protei.portal.core.model.ent.CaseStateWorkflow;
 import ru.protei.portal.core.model.ent.CaseStateWorkflowLink;
+import ru.protei.portal.core.model.helper.CollectionUtils;
 import ru.protei.portal.core.model.struct.CaseStateAndWorkflowList;
 import ru.protei.portal.ui.common.client.events.AuthEvents;
 import ru.protei.portal.ui.common.client.events.CaseStateEvents;
@@ -19,9 +20,7 @@ import ru.protei.portal.ui.common.client.widget.selector.base.SelectorWithModel;
 import ru.protei.portal.ui.common.shared.model.FluentCallback;
 
 import java.util.*;
-
-import static java.util.stream.Collectors.*;
-import static ru.protei.portal.core.model.dict.En_CaseStateWorkflow.NO_WORKFLOW;
+import java.util.stream.Collectors;
 
 /**
  * Модель статусов
@@ -72,82 +71,22 @@ public abstract class StateModel implements Activity {
                     fireEvent(new NotifyEvents.Show(lang.errGetList(), NotifyEvents.NotifyType.ERROR));
                 })
                 .withSuccess(caseStateAndWorkflowList -> {
+                    caseStateWorkflowList.clear();
+                    caseStateWorkflowList.addAll(caseStateAndWorkflowList.getCaseStateWorkflowList());
+                    caseStatesList.clear();
+                    caseStatesList.addAll(caseStateAndWorkflowList.getCaseStatesList());
                     isRefreshing = false;
-                    workflowCaseStateEdges = createWorkflowCaseStateEdges(
-                            caseStateAndWorkflowList.getCaseStatesList(),
-                            caseStateAndWorkflowList.getCaseStateWorkflowList());
                     notifySubscribers();
                 }));
     }
 
-    private Map<En_CaseStateWorkflow, Edges> createWorkflowCaseStateEdges(
-            List<CaseState> caseStatesList, List<CaseStateWorkflow> caseStateWorkflowList) {
-        Map<En_CaseStateWorkflow, Edges> workflowToEdges = new HashMap<>();
-
-        workflowToEdges.put(NO_WORKFLOW, createNoWorkflowMap(caseStatesList));
-
-        Map<Long, List<CaseStateWorkflowLink>> idToWorkflowList =
-                caseStateWorkflowList.stream().collect(
-                        toMap(CaseStateWorkflow::getId, CaseStateWorkflow::getCaseStateWorkflowLinks));
-
-        Comparator<En_CaseState> stateComparator = createStateComparator(caseStatesList);
-        for (En_CaseStateWorkflow en_caseStateWorkflow : En_CaseStateWorkflow.values()) {
-            if (en_caseStateWorkflow == NO_WORKFLOW) {
-                continue;
-            }
-            workflowToEdges.put(en_caseStateWorkflow,
-                    createOtherWorkflowMap(stateComparator, idToWorkflowList.get((long) en_caseStateWorkflow.getId())));
-        }
-
-        return workflowToEdges;
-    }
-
-    private Comparator<En_CaseState> createStateComparator(List<CaseState> caseStatesList) {
-        Map<Long, Integer> idToOrder =
-                caseStatesList.stream().collect(
-                        toMap(CaseState::getId, CaseState::getViewOrder));
-        return Comparator.comparing((En_CaseState en_CaseState) -> idToOrder.get((long) en_CaseState.getId()))
-                .thenComparing(En_CaseState::getId);
-    }
-
-    private Edges createNoWorkflowMap(List<CaseState> caseStatesList) {
-
-        List<En_CaseState> noWorkflowList = caseStatesList.stream().map(
-                caseState -> En_CaseState.getById(caseState.getId()))
-                .collect( toList());
-
-        Edges noWorkflowEdges = new Edges();
-        for (En_CaseState currentCaseState : En_CaseState.values()) {
-            if (!currentCaseState.isTerminalState()) {
-                noWorkflowEdges.addEdges(currentCaseState, noWorkflowList);
-            }
-        }
-        return noWorkflowEdges;
-    }
-
-    private Edges createOtherWorkflowMap(Comparator<En_CaseState> comparator,
-            List<CaseStateWorkflowLink> caseStateWorkflowList) {
-
-        Map<En_CaseState, Set<En_CaseState>> flowWithStateToOrder = caseStateWorkflowList.stream()
-                .collect(groupingBy(
-                        CaseStateWorkflowLink::getCaseStateFrom,
-                        mapping(CaseStateWorkflowLink::getCaseStateTo, toCollection(() -> new TreeSet<>(comparator)))
-                ));
-
-        Edges workflowEdges = new Edges();
-        for (En_CaseState en_caseState : flowWithStateToOrder.keySet()) {
-            workflowEdges.addEdges(en_caseState, new ArrayList<>(flowWithStateToOrder.get(en_caseState)));
-        }
-
-        return workflowEdges;
-    }
-
     private boolean isDataLoaded() {
-        return workflowCaseStateEdges != null;
+        return CollectionUtils.isNotEmpty(caseStatesList) && CollectionUtils.isNotEmpty(caseStateWorkflowList);
     }
 
     private void clearData() {
-        workflowCaseStateEdges = null;
+        caseStatesList.clear();
+        caseStateWorkflowList.clear();
     }
 
     private void notifySubscribers() {
@@ -167,7 +106,39 @@ public abstract class StateModel implements Activity {
     }
 
     private List<En_CaseState> fetchNextCaseStatesForWorkflow(En_CaseStateWorkflow workflow, En_CaseState currentCaseState) {
-        return workflowCaseStateEdges.get(workflow).getTo(currentCaseState);
+
+        if (workflow == En_CaseStateWorkflow.NO_WORKFLOW) {
+            if (currentCaseState.isTerminalState()) {
+                return Collections.singletonList(currentCaseState);
+            } else {
+                return caseStatesList.stream().map(caseState -> En_CaseState.getById(caseState.getId())).collect(Collectors.toList());
+            }
+        }
+
+        Set<CaseState> nextCaseStates = new HashSet<>();
+        Optional<CaseState> currentState = caseStatesList.stream().filter(state -> En_CaseState.getById(state.getId()) == currentCaseState).findFirst();
+        if (currentState.isPresent()) {
+            nextCaseStates.add(currentState.get());
+        }
+
+        Optional<CaseStateWorkflow> caseStateWorkflow = caseStateWorkflowList.stream()
+                .filter(csw -> csw.isMatched(workflow))
+                .findFirst();
+
+        if (caseStateWorkflow.isPresent()) {
+            for (CaseStateWorkflowLink caseStateWorkflowLink : caseStateWorkflow.get().getCaseStateWorkflowLinks()) {
+                if (caseStateWorkflowLink.getCaseStateFrom() != currentCaseState) {
+                    continue;
+                }
+
+                Optional<CaseState> caseState = caseStatesList.stream().filter(state -> En_CaseState.getById(state.getId()) == caseStateWorkflowLink.getCaseStateTo()).findFirst();
+                if (caseState.isPresent()) {
+                    nextCaseStates.add(caseState.get());
+                }
+            }
+        }
+
+        return nextCaseStates.stream().sorted(Comparator.comparingInt(CaseState::getViewOrder)).map(caseState -> En_CaseState.getById(caseState.getId())).collect(Collectors.toList());
     }
 
     @Inject
@@ -176,27 +147,16 @@ public abstract class StateModel implements Activity {
     CaseStateWorkflowControllerAsync caseStateWorkflowController;
 
     private boolean isRefreshing = false;
+    private List<CaseState> caseStatesList = new ArrayList<>();
+    private List<CaseStateWorkflow> caseStateWorkflowList = new ArrayList<>();
     private Map<SelectorWithModel<En_CaseState>, WorkflowWithState> subscriberMap = new HashMap<>();
-    private Map<En_CaseStateWorkflow, Edges> workflowCaseStateEdges;
 
-    private static class WorkflowWithState {
+    private class WorkflowWithState {
         En_CaseStateWorkflow workflow;
         En_CaseState state;
         WorkflowWithState(En_CaseStateWorkflow workflow, En_CaseState state) {
             this.workflow = workflow;
             this.state = state;
-        }
-    }
-
-    private static class Edges {
-        Map<En_CaseState, List<En_CaseState>> map = new HashMap<>();
-
-        void addEdges(En_CaseState from, List<En_CaseState> to) {
-            map.put(from, to);
-        }
-
-        List<En_CaseState> getTo(En_CaseState from) {
-            return map.getOrDefault(from, new ArrayList<>());
         }
     }
 }
