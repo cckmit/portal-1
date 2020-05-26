089199e9dd5e418786155b30ca92fce3dae671e3
porubov <devnull@localhost>
2020-05-26 15:58:20 +0300

    PORTAL-957_1. partial

--- module/model/src/main/java/ru/protei/portal/core/utils/JiraUtils.java
+++ module/model/src/main/java/ru/protei/portal/core/utils/JiraUtils.java
@@ -2,6 +2,7 @@ package ru.protei.portal.core.utils;
 
 import ru.protei.portal.core.model.ent.ExternalCaseAppData;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class JiraUtils {
@@ -40,7 +41,7 @@ public class JiraUtils {
     }
 
     public static Pattern getJiraImagePattern() {
-        return Pattern.compile("\\![^\\!]*\\!");    // !*!
+        return Pattern.compile("(^|\\s)![^!\\t\\n\\r]*[!]($|\\s)");    // !*!
     }
 
     public static class ImageNode {
@@ -50,7 +51,11 @@ public class JiraUtils {
 
     static public ImageNode parseImageNode(String imageString) {
         ImageNode imageNode = new ImageNode();
-        String[] split = imageString.split("\\|");
+        Pattern p = Pattern.compile("![^!]*!");
+        Matcher m = p.matcher(imageString);
+        m.find();
+        imageString = m.group();
+        String[] split = imageString.substring(1, imageString.length()-1).split("\\|");
         for (String part : split) {
             if (!part.contains("=")) {
                 imageNode.link = part;
@@ -63,5 +68,70 @@ public class JiraUtils {
 
         }
         return imageNode;
+    }
+
+    public static class JiraImageNode {
+        public String space;
+        public String page;
+        public String filename;
+        public int oldFilenameStart;
+        public int oldFilenameEnd;
+        public String alt;
+        public int oldAltStart;
+        public int oldAltEnd;
+    }
+
+    static public JiraImageNode parseJiraImageNode(String originalString) {
+        JiraImageNode node = new JiraImageNode();
+        if (originalString.length() >= 5 && originalString.charAt(0) != ')') {
+            int index = originalString.indexOf(124);
+            String resourceString;
+            String propertiesString;
+            if (index == -1) {
+                resourceString = originalString;
+                propertiesString = "";
+            } else {
+                resourceString = originalString.substring(0, index);
+                propertiesString = originalString.substring(index + 1);
+            }
+
+            if (resourceString.indexOf(58) != -1) {
+                node.space = resourceString.substring(0, resourceString.indexOf(58));
+                resourceString = resourceString.substring(resourceString.indexOf(58) + 1);
+            }
+
+            if (resourceString.indexOf(94) != -1) {
+                node.page = resourceString.substring(0, resourceString.indexOf(94));
+                resourceString = resourceString.substring(resourceString.indexOf(94) + 1);
+            }
+
+            node.filename = resourceString;
+
+            int dot_pos = resourceString.lastIndexOf(".");
+            if (dot_pos < 0) {
+                return null;
+            } else {
+                String file_ext = resourceString.substring(dot_pos + 1);
+                if (file_ext.length() == 0) {
+                    return null;
+                } else {
+                    if (!file_ext.toLowerCase().matches("jpg|jpeg|png|gif")) {
+                        return null;
+                    }
+                }
+            }
+
+            for (String part : propertiesString.split("\\|")) {
+                String[] split1 = part.split("=");
+                switch (split1[0].toUpperCase().trim()) {
+                    case "ALT":
+                        node.alt = split1[1];
+                        break;
+                }
+            }
+            return node;
+        } else {
+            return null;
+        }
     }
 }
