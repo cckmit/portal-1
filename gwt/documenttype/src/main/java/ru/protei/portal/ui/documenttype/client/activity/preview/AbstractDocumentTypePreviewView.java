0e0a37bca21882a71f8a38c6edddf643fad183b8
porubov <devnull@localhost>
2020-05-28 13:41:11 +0300

    4) Виды документов, карточка создания, поле "Краткое название" не имеет ограничений по длине, в базе это VarChar(16)

--- gwt/documenttype/src/main/java/ru/protei/portal/ui/documenttype/client/activity/preview/AbstractDocumentTypePreviewView.java
+++ gwt/documenttype/src/main/java/ru/protei/portal/ui/documenttype/client/activity/preview/AbstractDocumentTypePreviewView.java
@@ -3,6 +3,7 @@ package ru.protei.portal.ui.documenttype.client.activity.preview;
 import com.google.gwt.user.client.ui.HasValue;
 import com.google.gwt.user.client.ui.IsWidget;
 import ru.protei.portal.core.model.dict.En_DocumentCategory;
+import ru.protei.portal.ui.common.client.widget.validatefield.HasValidable;
 
 public interface AbstractDocumentTypePreviewView extends IsWidget {
 
@@ -15,4 +16,10 @@ public interface AbstractDocumentTypePreviewView extends IsWidget {
     HasValue<En_DocumentCategory> category();
 
     HasValue<String> gost();
+
+    HasValidable nameValidation();
+
+    HasValidable shortNameValidation();
+
+    HasValidable gostValidation();
 }
