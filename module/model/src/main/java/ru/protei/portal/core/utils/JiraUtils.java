af5a4fafb350b2c5189b64863c21e3b2740eefee
porubov <devnull@localhost>
2020-05-29 13:16:45 +0300

    rework to EmbeddedResourceParser

--- module/model/src/main/java/ru/protei/portal/core/utils/JiraUtils.java
+++ module/model/src/main/java/ru/protei/portal/core/utils/JiraUtils.java
@@ -1,15 +1,11 @@
 package ru.protei.portal.core.utils;
 
+import com.atlassian.renderer.embedded.EmbeddedResourceParser;
 import ru.protei.portal.core.model.ent.Attachment;
 import ru.protei.portal.core.model.ent.CaseAttachment;
 import ru.protei.portal.core.model.ent.CaseComment;
 import ru.protei.portal.core.model.ent.ExternalCaseAppData;
 
-import javax.activation.FileTypeMap;
-import javax.activation.MimetypesFileTypeMap;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.*;
 import java.util.function.BiFunction;
 import java.util.function.BiPredicate;
@@ -19,37 +15,7 @@ import java.util.regex.Pattern;
 import static ru.protei.portal.core.model.helper.CaseCommentUtils.makeJiraImageString;
 
 public class JiraUtils {
-    private static FileTypeMap fileTypeMap;
     private static Pattern pattern = Pattern.compile("((?<![\\p{L}\\p{Nd}\\\\])|(?<=inltokxyzkdtnhgnsbdfinltok))\\!([^\\s\\!]((?!\\!)[\\p{L}\\p{Nd}\\p{Z}\\p{S}\\p{M}\\p{P}]*?[^\\s\\!])?)(?<!\\\\)\\!((?![\\p{L}\\p{Nd}])|(?=inltokxyzkdtnhgnsbdfinltok))");;
-
-    public static URL getResource(String resourceName, Class<?> callingClass) {
-        URL url = null;
-        url = Thread.currentThread().getContextClassLoader().getResource(resourceName);
-        if (url == null) {
-            url = JiraUtils.class.getClassLoader().getResource(resourceName);
-        }
-
-        if (url == null) {
-            url = callingClass.getClassLoader().getResource(resourceName);
-        }
-
-        return url;
-    }
-
-    public static InputStream getResourceAsStream(String resourceName, Class<?> callingClass) {
-        URL url = getResource(resourceName, callingClass);
-
-        try {
-            return url != null ? url.openStream() : null;
-        } catch (IOException var4) {
-            return null;
-        }
-    }
-
-    static {
-        InputStream mimeTypesStream = getResourceAsStream("mime.types", JiraUtils.class);
-        fileTypeMap = new MimetypesFileTypeMap(mimeTypesStream);
-    }
 
     public static JiraIssueData convert(ExternalCaseAppData appData) {
         return new JiraIssueData(extractEndpointId(appData), extractIssueKey(appData), extractIssueProjectId(appData));
@@ -89,61 +55,16 @@ public class JiraUtils {
         public String alt;
     }
 
-    static private ImageNode parseImageNode(String originalString) {
-        ImageNode node = new ImageNode();
-        // copypasta EmbeddedResourceParser.class
+    static private ImageNode parseImageNodeByJira(String originalString) {
         if (originalString.length() >= 5 && originalString.charAt(0) != ')') {
-            int index = originalString.indexOf(124);
-            // find resourceString, propertiesString
-            String resourceString;
-            String propertiesString;
-            if (index == -1) {
-                resourceString = originalString;
-                propertiesString = "";
-            } else {
-                resourceString = originalString.substring(0, index);
-                propertiesString = originalString.substring(index + 1);
-            }
+            EmbeddedResourceParser embeddedResourceParser = new EmbeddedResourceParser(originalString);
+            ImageNode node = new ImageNode();
+            node.filename = embeddedResourceParser.getFilename();
+            node.alt = embeddedResourceParser.getProperties().getProperty("alt");
 
-            if (resourceString.indexOf(58) != -1) {
-                // find space
-                resourceString = resourceString.substring(resourceString.indexOf(58) + 1);
-            }
-
-            if (resourceString.indexOf(94) != -1) {
-                // find page
-                resourceString = resourceString.substring(resourceString.indexOf(94) + 1);
-            }
-
-            // find filename
-            node.filename = resourceString;
-
-            // check file_ext
-            String type = fileTypeMap.getContentType(resourceString);
-            if (!type.startsWith("image")) {
-                return null;
-            }
-
-            // parse params
-            StringTokenizer st = new StringTokenizer(propertiesString, ",");
-            while(st.hasMoreTokens()) {
-                String paramPair = st.nextToken();
-                if (paramPair.indexOf(61) > 0) {
-                    String paramName = paramPair.substring(0, paramPair.indexOf(61)).trim();
-                    String paramValue = paramPair.substring(paramPair.indexOf(61) + 1).trim();
-                    if (paramValue.startsWith("\"") && paramValue.endsWith("\"")) {
-                        paramValue = paramValue.substring(1, paramValue.length() - 1);
-                    }
-
-                    if ("alt".equals(paramName.toLowerCase())) {
-                        node.alt = paramValue;
-                    }
-                }
-            }
             return node;
-        } else {
-            return null;
         }
+        return null;
     }
 
     static public String getTextWithReplacedImages(String text, Collection<Attachment> attachments,
@@ -154,26 +75,26 @@ public class JiraUtils {
         if (!matcher.find()) {
             resultText = text;
         } else {
-            StringBuilder buffer = new StringBuilder();
+            StringBuilder builder = new StringBuilder();
             int mark = 0;
 
             do {
-                buffer.append(text, mark, matcher.start());
+                builder.append(text, mark, matcher.start());
                 mark = matcher.end();
                 String originalString = matcher.group(2);
-                Optional<String> imageString = Optional.ofNullable(parseImageNode(originalString))
+                Optional<String> imageString = Optional.ofNullable(parseImageNodeByJira(originalString))
                         .flatMap(node -> attachments.stream()
                                 .filter(attachment -> attachmentFilter.test(node, attachment))
                                 .max(Comparator.comparing(ru.protei.portal.core.model.ent.Attachment::getCreated))
                                 .map(attachment -> makeImageString.apply(node, attachment)));
                 if (imageString.isPresent()) {
-                    buffer.append(imageString.get());
+                    builder.append(imageString.get());
                 } else {
-                    buffer.append('!').append(originalString).append('!');
+                    builder.append('!').append(originalString).append('!');
                 }
             } while (matcher.find());
 
-            resultText = buffer.append(text, mark, text.length()).toString();
+            resultText = builder.append(text, mark, text.length()).toString();
         }
         return resultText;
     }
