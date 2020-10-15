3d3d0938a9c2d7becb36c810726ea744750fe786
Evgeny Fomin <special_for_web@mail.ru>
2020-10-15 13:35:05 +0300

    PORTAL-1441: ArrowSelectableSelectorPopup refactoring, close branch

--- gwt/common/src/main/java/ru/protei/portal/ui/common/client/selector/popup/arrowselectable/ArrowSelectableSelectorPopup.java
+++ gwt/common/src/main/java/ru/protei/portal/ui/common/client/selector/popup/arrowselectable/ArrowSelectableSelectorPopup.java
@@ -14,43 +14,41 @@ public class ArrowSelectableSelectorPopup extends SelectorPopupWithSearch {
     public ArrowSelectableSelectorPopup(TextAreaHandler textAreaHandler) {
         childContainer.addStyleName("arrow-selectable");
         childContainer.addDomHandler(event -> textAreaHandler.focusTextArea(), MouseOverEvent.getType());
-        childContainer.addDomHandler(createPopupKeyDownHandler(childContainer, textAreaHandler), KeyDownEvent.getType());
+        childContainer.addDomHandler(event -> createPopupKeyDownHandler(event, childContainer, textAreaHandler), KeyDownEvent.getType());
     }
 
     public void focus() {
         focusWidget(getNext(childContainer, null));
     }
 
-    private KeyDownHandler createPopupKeyDownHandler(final ComplexPanel childContainer, TextAreaHandler textAreaHandler) {
-        return event -> {
-            if (KeyCodes.KEY_ESCAPE == event.getNativeKeyCode()) {
-                event.preventDefault();
+    private void createPopupKeyDownHandler(KeyDownEvent event, ComplexPanel childContainer, TextAreaHandler textAreaHandler) {
+        if (KeyCodes.KEY_ESCAPE == event.getNativeKeyCode()) {
+            event.preventDefault();
+            textAreaHandler.focusTextArea();
+            return;
+        }
+
+        if (KeyCodes.KEY_UP == event.getNativeKeyCode()) {
+            event.preventDefault();
+
+            if (focusWidget(getPrevious(childContainer, currentWidget)) == null) {
                 textAreaHandler.focusTextArea();
-                return;
             }
 
-            if (KeyCodes.KEY_UP == event.getNativeKeyCode()) {
-                event.preventDefault();
+            return;
+        }
 
-                if (focusWidget(getPrevious(childContainer, currentWidget)) == null) {
-                    textAreaHandler.focusTextArea();
-                }
-
-                return;
-            }
-
-            if (KeyCodes.KEY_DOWN == event.getNativeKeyCode()) {
-                event.preventDefault();
-                focusWidget(getNext(childContainer, currentWidget));
-                return;
-            }
+        if (KeyCodes.KEY_DOWN == event.getNativeKeyCode()) {
+            event.preventDefault();
+            focusWidget(getNext(childContainer, currentWidget));
+            return;
+        }
 
 //            Если нажали не на Enter, а продолжили ввод, то переводим фокус обратно
-            if (KeyCodes.KEY_ENTER != event.getNativeKeyCode()) {
-                textAreaHandler.focusTextArea();
-                textAreaHandler.onValueChanged();
-            }
-        };
+        if (KeyCodes.KEY_ENTER != event.getNativeKeyCode()) {
+            textAreaHandler.focusTextArea();
+            textAreaHandler.onValueChanged();
+        }
     }
 
     private Widget getNext(final ComplexPanel childContainer, final Widget currentWidget) {
