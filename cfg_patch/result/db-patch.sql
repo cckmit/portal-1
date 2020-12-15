6abecf38831363ec3cd2621034e9f9f0dcbc7c31
Evgeny Fomin <special_for_web@mail.ru>
2020-12-15 10:59:03 +0300

    PORTAL-1602: add case_filter update as liquibase changeset

--- cfg_patch/result/db-patch.sql
+++ cfg_patch/result/db-patch.sql
@@ -1,4 +1,1 @@
 use portal;
-
-# Переходим на идентификаторы. В базе автоматически будут заменены строковые значения енамов на айди.
-update case_filter set params = REGEXP_REPLACE(params, 'importances', 'importanceIds') where params like "%importances%";
