7189f89b2778f0f159e715fca0bcecb5db3d028a
turik <devnull@localhost>
2020-12-21 00:50:41 +0300

    PORTAL-1586 - Changed getting html

--- gwt/common/src/main/java/ru/protei/portal/ui/common/client/activity/externallink/ExternalLinkActivity.java
+++ gwt/common/src/main/java/ru/protei/portal/ui/common/client/activity/externallink/ExternalLinkActivity.java
@@ -1,14 +1,21 @@
 package ru.protei.portal.ui.common.client.activity.externallink;
 
+import com.google.inject.Inject;
 import ru.brainworm.factory.generator.activity.client.activity.Activity;
 import ru.brainworm.factory.generator.activity.client.annotations.Event;
 import ru.protei.portal.ui.common.client.events.AppEvents;
 import ru.protei.portal.ui.common.client.events.AuthEvents;
+import ru.protei.portal.ui.common.client.service.AppServiceAsync;
+import ru.protei.portal.ui.common.shared.model.FluentCallback;
 
 public abstract class ExternalLinkActivity implements Activity {
 
     @Event
     public void onAuthSuccess(AuthEvents.Success event) {
-        fireEvent(new AppEvents.InitExternalLinks());
+        appService.getExternalLinksHtml(new FluentCallback<String>()
+                .withSuccess(s -> fireEvent(new AppEvents.InitExternalLinks(s))));
     }
+
+    @Inject
+    AppServiceAsync appService;
 }
