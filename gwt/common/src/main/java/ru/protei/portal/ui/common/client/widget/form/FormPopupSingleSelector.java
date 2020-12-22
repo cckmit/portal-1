49040a082d3b0c57f8b3e3d83343cb9b26bafd3a
novikov_a <devnull@localhost>
2020-12-22 17:31:42 +0300

    PORTAL-1610 added title for 'issueInitiatorInfo' field

--- gwt/common/src/main/java/ru/protei/portal/ui/common/client/widget/form/FormPopupSingleSelector.java
+++ gwt/common/src/main/java/ru/protei/portal/ui/common/client/widget/form/FormPopupSingleSelector.java
@@ -64,6 +64,9 @@ public class FormPopupSingleSelector<T> extends AbstractPopupSelector<T>
         if(isValidable)
             setValid( isValid() );
 
+        if (hasTitle)
+            text.setTitle(getRendered(value));
+
         getPopup().hide();
         ValueChangeEvent.fire(this, value);
     }
@@ -83,6 +86,9 @@ public class FormPopupSingleSelector<T> extends AbstractPopupSelector<T>
         }
         if(isValidable)
             setValid( isValid() );
+
+        if (hasTitle)
+            text.setTitle(getRendered(selectorValue));
     }
 
     @Override
@@ -151,6 +157,10 @@ public class FormPopupSingleSelector<T> extends AbstractPopupSelector<T>
         formContainer.removeStyleName(REQUIRED_STYLENAME);
     }
 
+    public void setHasTitle(boolean hasTitle) {
+        this.hasTitle = hasTitle;
+    }
+
     public void onShowPopupClicked(HTMLPanel button) {
         getPopup().getChildContainer().clear();
         getSelector().fillFromBegin(this);
@@ -183,7 +193,11 @@ public class FormPopupSingleSelector<T> extends AbstractPopupSelector<T>
     }
 
     protected void showValue( T value) {
-        this.text.setInnerHTML(selectedValueRenderer.render(value));
+        this.text.setInnerHTML(getRendered(value));
+    }
+
+    private String getRendered(T value) {
+       return selectedValueRenderer.render(value);
     }
 
     @Override
@@ -225,6 +239,7 @@ public class FormPopupSingleSelector<T> extends AbstractPopupSelector<T>
     }
     private SingleValuePageableSelector<T> selector = new SingleValuePageableSelector<T>();
     private boolean isValidable;
+    private boolean hasTitle;
     private SelectedValueRenderer<T> selectedValueRenderer = selector::makeElementHtml;
 
     private static final String ERROR_STYLENAME ="has-error";
