a0715b1fd30f0bf79b8e21c95c8896b79f436065
novikov_a <devnull@localhost>
2021-03-16 20:53:28 +0300

    PORTAL-1680 changed range of absence time for absence type 'NIGHT_WORK'

--- gwt/absence/src/main/java/ru/protei/portal/ui/absence/client/view/common/AbsenceCommonView.java
+++ gwt/absence/src/main/java/ru/protei/portal/ui/absence/client/view/common/AbsenceCommonView.java
@@ -3,20 +3,27 @@ package ru.protei.portal.ui.absence.client.view.common;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.debug.client.DebugInfo;
 import com.google.gwt.dom.client.LabelElement;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.*;
 import com.google.inject.Inject;
+import ru.brainworm.factory.core.datetimepicker.shared.dto.DateInterval;
 import ru.protei.portal.core.model.dict.En_AbsenceReason;
 import ru.protei.portal.core.model.view.PersonShortView;
 import ru.protei.portal.test.client.DebugIds;
 import ru.protei.portal.ui.absence.client.activity.common.AbstractAbsenceCommonView;
+import ru.protei.portal.ui.absence.client.activity.create.AbstractAbsenceCreateView;
+import ru.protei.portal.ui.absence.client.activity.edit.AbstractAbsenceEditView;
 import ru.protei.portal.ui.common.client.lang.Lang;
 import ru.protei.portal.ui.common.client.widget.autoresizetextarea.AutoResizeTextArea;
 import ru.protei.portal.ui.common.client.widget.loading.IndeterminateCircleLoading;
 import ru.protei.portal.ui.common.client.widget.selector.absencereason.AbsenceReasonButtonSelector;
 import ru.protei.portal.ui.common.client.widget.selector.person.EmployeeButtonSelector;
 import ru.protei.portal.ui.common.client.widget.validatefield.HasValidable;
+
+import java.util.*;
 
 public class AbsenceCommonView extends Composite implements AbstractAbsenceCommonView {
 
@@ -76,6 +83,27 @@ public class AbsenceCommonView extends Composite implements AbstractAbsenceCommo
         return dateContainer;
     }
 
+    @UiHandler("reason")
+    public void onReasonChanged(ValueChangeEvent<En_AbsenceReason> event) {
+        List<DateInterval> newDateRange = new LinkedList<>();
+        createView.dateRange().getValue().forEach(interval -> {
+            Date to = interval.to;
+            if (event.getValue().equals(En_AbsenceReason.NIGHT_WORK)) {
+                to.setHours(13);
+                to.setMinutes(00);
+                to.setSeconds(00);
+            } else {
+                to.setHours(23);
+                to.setMinutes(59);
+                to.setSeconds(59);
+            }
+
+            newDateRange.add(new DateInterval(interval.from, to));
+        });
+
+        createView.dateRange().setValue(newDateRange);
+    }
+
     protected void ensureDebugIds() {
         if (!DebugInfo.isDebugIdEnabled()) {
             return;
@@ -117,6 +145,11 @@ public class AbsenceCommonView extends Composite implements AbstractAbsenceCommo
     @Inject
     @UiField
     Lang lang;
+
+    @Inject
+    AbstractAbsenceCreateView createView;
+    @Inject
+    AbstractAbsenceEditView editView;
 
     private static AbsenceCommonViewUiBinder ourUiBinder = GWT.create(AbsenceCommonViewUiBinder.class);
     interface AbsenceCommonViewUiBinder extends UiBinder<HTMLPanel, AbsenceCommonView> {}
