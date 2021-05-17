2ae7aacef74a5a48c7d01f84b5683f7a1a2b6951
turik <devnull@localhost>
2021-05-17 16:41:53 +0300

    PORTAL-1859 - Fixed assignment, added bootstrap-method for migration person with not active worker-entry

--- module/core/src/main/java/ru/protei/portal/core/service/bootstrap/BootstrapServiceImpl.java
+++ module/core/src/main/java/ru/protei/portal/core/service/bootstrap/BootstrapServiceImpl.java
@@ -1,66 +1,25 @@
 package ru.protei.portal.core.service.bootstrap;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.commons.lang3.time.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.GenericBeanDefinition;
-import org.springframework.context.ApplicationContext;
 import org.springframework.transaction.annotation.Transactional;
-import ru.protei.portal.config.PortalConfig;
-import ru.protei.portal.core.index.document.DocumentStorageIndex;
 import ru.protei.portal.core.model.dao.*;
-import ru.protei.portal.core.model.dao.impl.PortalBaseJdbcDAO;
 import ru.protei.portal.core.model.dict.*;
-import ru.protei.portal.core.model.dto.ReportCaseQuery;
 import ru.protei.portal.core.model.ent.*;
 import ru.protei.portal.core.model.helper.CollectionUtils;
-import ru.protei.portal.core.model.helper.HelperFunc;
-import ru.protei.portal.core.model.helper.PhoneUtils;
-import ru.protei.portal.core.model.helper.StringUtils;
 import ru.protei.portal.core.model.query.*;
-import ru.protei.portal.core.model.struct.ContactInfo;
 import ru.protei.portal.core.model.struct.ContactItem;
-import ru.protei.portal.core.model.struct.DateRange;
-import ru.protei.portal.core.model.struct.Pair;
 import ru.protei.portal.core.model.util.CrmConstants;
-import ru.protei.portal.core.model.util.sqlcondition.Condition;
-import ru.protei.portal.core.model.view.PersonShortView;
-import ru.protei.portal.core.service.YoutrackService;
-import ru.protei.portal.core.svn.document.DocumentSvnApi;
-import ru.protei.portal.tools.migrate.struct.ExternalPersonAbsence;
-import ru.protei.portal.tools.migrate.struct.ExternalPersonLeave;
-import ru.protei.portal.tools.migrate.struct.ExternalReservedIp;
-import ru.protei.portal.tools.migrate.struct.ExternalSubnet;
-import ru.protei.portal.tools.migrate.sybase.LegacySystemDAO;
-import ru.protei.winter.core.utils.beans.SearchResult;
-import ru.protei.winter.jdbc.JdbcBaseDAO;
-import ru.protei.winter.jdbc.JdbcDAO;
-import ru.protei.winter.jdbc.JdbcManyRelationsHelper;
-import ru.protei.winter.jdbc.JdbcObjectMapperRegistrator;
-import ru.protei.winter.jdbc.annotations.ConverterType;
-import ru.protei.winter.jdbc.annotations.JdbcColumn;
-import ru.protei.winter.jdbc.annotations.JdbcEntity;
-import ru.protei.winter.jdbc.annotations.JdbcId;
 
-import javax.inject.Inject;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
 import static ru.protei.portal.core.model.helper.CollectionUtils.*;
 import static ru.protei.portal.core.model.helper.CollectionUtils.toList;
-import static ru.protei.portal.core.model.util.sqlcondition.SqlQueryBuilder.condition;
-import static ru.protei.portal.core.model.util.sqlcondition.SqlQueryBuilder.query;
 
 /**
  * Сервис выполняющий первичную инициализацию, работу с исправлением данных
@@ -89,10 +48,42 @@ public class BootstrapServiceImpl implements BootstrapService {
             this.changePersonToSingleCompany();
             bootstrapAppDAO.createAction("changePersonToSingleCompany");
         }
+
+        if (!bootstrapAppDAO.isActionExists("changePersonToSingleCompanyForNotActiveWorkerEntry")) {
+            this.changePersonToSingleCompanyForNotActiveWorkerEntry();
+            bootstrapAppDAO.createAction("changePersonToSingleCompanyForNotActiveWorkerEntry");
+        }
         /**
          *  end Спринт 72 */
 
         log.info( "bootstrapApplication(): BootstrapService complete."  );
+    }
+
+    private void changePersonToSingleCompanyForNotActiveWorkerEntry() {
+
+        log.debug("changePersonToSingleCompanyForNotActiveWorkerEntry(): start");
+
+        List<Long> notActiveWorkerEntryPersonIds =
+                workerEntryDAO.partialGetListByCondition("active = 0 and personId in (SELECT personId from worker_entry GROUP BY personId HAVING COUNT(*) < 2)",
+                        null, "personId").stream().map(workerEntry -> workerEntry.getPersonId()).collect(toList());
+
+        List<Company> companies = companyDAO.getSingleHomeCompanies();
+        for (Company company : companies) {
+
+            if (CrmConstants.Company.HOME_COMPANY_ID == company.getId()) {
+                continue;
+            }
+
+            List<WorkerEntry> workerEntryShortViewList = workerEntryDAO.partialGetByPersonIds(notActiveWorkerEntryPersonIds, company.getId());
+            List<Long> personIds = workerEntryShortViewList.stream()
+                    .map(WorkerEntry::getPersonId)
+                    .distinct()
+                    .collect(Collectors.toList());
+
+            changePersonToSingleCompany(personIds, company.getId());
+        }
+
+        log.debug("changePersonToSingleCompanyForNotActiveWorkerEntry(): finish");
     }
 
     private void changePersonToSingleCompany() {
@@ -113,68 +104,72 @@ public class BootstrapServiceImpl implements BootstrapService {
                     .distinct()
                     .collect(Collectors.toList());
 
-            if (CollectionUtils.isEmpty(personIds)) {
-                continue;
-            }
-
-            // Update person company
-            personIds.forEach(personId -> {
-                personDAO.partialMerge(new Person(personId, company.getId()), "company_id");
-                log.info("changePersonToSingleCompany(): person with id={} updated", personId);
-            });
-
-            // Update manager company of issue
-            CaseQuery caseQuery = new CaseQuery();
-            caseQuery.setType(En_CaseType.CRM_SUPPORT);
-            caseQuery.setManagerIds(personIds);
-            caseShortViewDAO.listByQuery(caseQuery).forEach(caseShortView -> {
-                caseShortView.setManagerCompanyId(company.getId());
-                caseShortViewDAO.partialMerge(caseShortView, "manager_company_id");
-                log.info("changePersonToSingleCompany(): issue with id={} updated", caseShortView.getId());
-            });
-
-            // Update manager company of filter
-            List<CaseFilter> filters = caseFilterDAO.getListByCondition("params like ? and type = ?", "%managerIds%", En_CaseFilterType.CASE_OBJECTS.name());
-            for (CaseFilter filter : filters) {
-                try {
-                    CaseQuery query = objectMapper.readValue(filter.getParams(), CaseQuery.class);
-                    List<Long> managerCompanyIds = emptyIfNull(query.getManagerCompanyIds());
-                    if (emptyIfNull(query.getManagerIds()).stream().anyMatch(personIds::contains) &&
-                            !managerCompanyIds.contains(company.getId())) {
-                        managerCompanyIds.add(company.getId());
-                        query.setManagerCompanyIds(managerCompanyIds);
-                        filter.setParams(objectMapper.writeValueAsString(query));
-                        caseFilterDAO.partialMerge(filter, "params");
-                        log.info("changePersonToSingleCompany(): filter with id={} updated", filter.getId());
-                    }
-                } catch (IOException e) {
-                    log.warn("changePersonToSingleCompany(): cannot update filter with id={}", filter.getId());
-                    continue;
-                }
-            }
-
-            // Update manager company of report
-            List<Report> reports = reportDAO.getListByCondition("case_query like ? and type=? and is_removed=?", "%managerIds%", En_ReportType.CASE_OBJECTS.name(), false);
-            for (Report report : reports) {
-                try {
-                    CaseQuery query = objectMapper.readValue(report.getQuery(), CaseQuery.class);
-                    List<Long> managerCompanyIds = emptyIfNull(query.getManagerCompanyIds());
-                    if (emptyIfNull(query.getManagerIds()).stream().anyMatch(personIds::contains) &&
-                            !managerCompanyIds.contains(company.getId())) {
-                        managerCompanyIds.add(company.getId());
-                        query.setManagerCompanyIds(managerCompanyIds);
-                        report.setQuery(objectMapper.writeValueAsString(query));
-                        reportDAO.partialMerge(report, "case_query");
-                        log.info("changePersonToSingleCompany(): report with id={} updated", report.getId());
-                    }
-                } catch (IOException e) {
-                    log.warn("changePersonToSingleCompany(): cannot update report with id={}", report.getId());
-                    continue;
-                }
-            }
+            changePersonToSingleCompany(personIds, company.getId());
         }
 
         log.debug("changePersonToSingleCompany(): finish");
+    }
+
+    private void changePersonToSingleCompany(List<Long> personIds, Long companyId) {
+        if (CollectionUtils.isEmpty(personIds)) {
+            return;
+        }
+
+        // Update person company
+        personIds.forEach(personId -> {
+            personDAO.partialMerge(new Person(personId, companyId), "company_id");
+            log.info("changePersonToSingleCompany(): person with id={} updated", personId);
+        });
+
+        // Update manager company of issue
+        CaseQuery caseQuery = new CaseQuery();
+        caseQuery.setType(En_CaseType.CRM_SUPPORT);
+        caseQuery.setManagerIds(personIds);
+        caseShortViewDAO.listByQuery(caseQuery).forEach(caseShortView -> {
+            caseShortView.setManagerCompanyId(companyId);
+            caseShortViewDAO.partialMerge(caseShortView, "manager_company_id");
+            log.info("changePersonToSingleCompany(): issue with id={} updated", caseShortView.getId());
+        });
+
+        // Update manager company of filter
+        List<CaseFilter> filters = caseFilterDAO.getListByCondition("params like ? and type = ?", "%managerIds%", En_CaseFilterType.CASE_OBJECTS.name());
+        for (CaseFilter filter : filters) {
+            try {
+                CaseQuery query = objectMapper.readValue(filter.getParams(), CaseQuery.class);
+                List<Long> managerCompanyIds = emptyIfNull(query.getManagerCompanyIds());
+                if (emptyIfNull(query.getManagerIds()).stream().anyMatch(personIds::contains) &&
+                        !managerCompanyIds.contains(companyId)) {
+                    managerCompanyIds.add(companyId);
+                    query.setManagerCompanyIds(managerCompanyIds);
+                    filter.setParams(objectMapper.writeValueAsString(query));
+                    caseFilterDAO.partialMerge(filter, "params");
+                    log.info("changePersonToSingleCompany(): filter with id={} updated", filter.getId());
+                }
+            } catch (IOException e) {
+                log.warn("changePersonToSingleCompany(): cannot update filter with id={}", filter.getId());
+                continue;
+            }
+        }
+
+        // Update manager company of report
+        List<Report> reports = reportDAO.getListByCondition("case_query like ? and type=? and is_removed=?", "%managerIds%", En_ReportType.CASE_OBJECTS.name(), false);
+        for (Report report : reports) {
+            try {
+                CaseQuery query = objectMapper.readValue(report.getQuery(), CaseQuery.class);
+                List<Long> managerCompanyIds = emptyIfNull(query.getManagerCompanyIds());
+                if (emptyIfNull(query.getManagerIds()).stream().anyMatch(personIds::contains) &&
+                        !managerCompanyIds.contains(companyId)) {
+                    managerCompanyIds.add(companyId);
+                    query.setManagerCompanyIds(managerCompanyIds);
+                    report.setQuery(objectMapper.writeValueAsString(query));
+                    reportDAO.partialMerge(report, "case_query");
+                    log.info("changePersonToSingleCompany(): report with id={} updated", report.getId());
+                }
+            } catch (IOException e) {
+                log.warn("changePersonToSingleCompany(): cannot update report with id={}", report.getId());
+                continue;
+            }
+        }
     }
 
     private void updateContactItemsAccessType() {
