9a5dc9415bb0bed9b3affd5563010561da9034b5
butusov <devnull@localhost>
2021-05-17 16:32:53 +0300

    remove unused fields from UITS model

--- module/model/src/main/java/ru/protei/portal/core/model/ent/UitsIssueInfo.java
+++ module/model/src/main/java/ru/protei/portal/core/model/ent/UitsIssueInfo.java
@@ -1,20 +1,13 @@
 package ru.protei.portal.core.model.ent;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import ru.protei.portal.core.model.struct.Pair;
-
 import java.io.Serializable;
-import java.util.List;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class UitsIssueInfo implements Serializable {
 
     private String id;
     private String summary;
     private String description;
     private CaseState state;
-    private List<CaseComment> comments;
-    private List<Pair<Attachment, CaseAttachment>> attachments;
 
     public String getId() {
         return id;
@@ -48,31 +41,13 @@ public class UitsIssueInfo implements Serializable {
         this.state = state;
     }
 
-    public List<CaseComment> getComments() {
-        return comments;
-    }
-
-    public void setComments(List<CaseComment> comments) {
-        this.comments = comments;
-    }
-
-    public List<Pair<Attachment, CaseAttachment>> getAttachments() {
-        return attachments;
-    }
-
-    public void setAttachments(List<Pair<Attachment, CaseAttachment>> attachments) {
-        this.attachments = attachments;
-    }
-
     @Override
     public String toString() {
         return "YouTrackIssueInfo{" +
                 "stateId=" + state +
                 ", id='" + id + '\'' +
                 ", summary='" + summary + '\'' +
-                ", description='" + description + '\'' +
-                ", comments=" + comments +
-                ", attachments=" + attachments +
+                ", description='" + description +
                 '}';
     }
 }
