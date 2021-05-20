0622b7b00922db4902bb7c416bf0482686ab3d02
turik <devnull@localhost>
2021-05-20 13:48:32 +0300

    PORTAL-1858 - Added test api methods

--- module/test-api/src/main/java/ru/protei/portal/test/api/PortalTestApiController.java
+++ module/test-api/src/main/java/ru/protei/portal/test/api/PortalTestApiController.java
@@ -1,11 +1,57 @@
 package ru.protei.portal.test.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import ru.protei.portal.schedule.PortalScheduleTasks;
 
 @RestController
 @RequestMapping(value = "/test-api", headers = "Accept=application/json")
 @EnableWebMvc
 public class PortalTestApiController {
+
+    @Autowired
+    PortalScheduleTasks portalScheduleTasks;
+
+    @GetMapping(value = "/case-filter/notification")
+    public void processPersonCaseFilterMailNotification () {
+        portalScheduleTasks.processPersonCaseFilterMailNotification();
+    }
+
+    @GetMapping(value = "/daily-report/notification")
+    public void processScheduledMailReportsDaily () {
+        portalScheduleTasks.processScheduledMailReportsDaily();
+    }
+
+    @GetMapping(value = "/weekly-report/notification")
+    public void processScheduledMailReportsWeekly () {
+        portalScheduleTasks.processScheduledMailReportsWeekly();
+    }
+
+    @GetMapping(value = "/release-ip/notification")
+    public void remindAboutNeedToReleaseIp () {
+        portalScheduleTasks.remindAboutNeedToReleaseIp();
+    }
+
+    @GetMapping(value = "/employee-probation-period/notification")
+    public void remindAboutEmployeeProbationPeriod () {
+        portalScheduleTasks.remindAboutEmployeeProbationPeriod();
+    }
+
+    @GetMapping(value = "/contract-date/notification")
+    public void notifyAboutContractDates () {
+        portalScheduleTasks.notifyAboutContractDates();
+    }
+
+    @GetMapping(value = "/birthday/notification")
+    public void notifyAboutBirthdays () {
+        portalScheduleTasks.notifyAboutBirthdays();
+    }
+
+    @GetMapping(value = "/technical-support-validity/notification")
+    public void notifyExpiringTechnicalSupportValidity () {
+        portalScheduleTasks.notifyExpiringTechnicalSupportValidity();
+    }
 }
