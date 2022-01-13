28e8923853237117d2fd01885f0167024c5c042d
nunaev <nunaev@protei.ru>
2022-01-13 20:46:26 +0300

    PORTAL-2077 fix query

--- module/model/src/main/java/ru/protei/portal/core/model/dao/impl/CaseCommentTimeElapsedSumDAO_Impl.java
+++ module/model/src/main/java/ru/protei/portal/core/model/dao/impl/CaseCommentTimeElapsedSumDAO_Impl.java
@@ -5,12 +5,11 @@ import org.slf4j.LoggerFactory;
 import ru.protei.portal.core.model.dao.CaseCommentTimeElapsedSumDAO;
 import ru.protei.portal.core.model.dict.En_TimeElapsedType;
 import ru.protei.portal.core.model.ent.CaseCommentTimeElapsedSum;
-import ru.protei.portal.core.model.helper.CollectionUtils;
 import ru.protei.portal.core.model.query.CaseQuery;
 import ru.protei.portal.core.model.struct.Interval;
+import ru.protei.portal.core.model.util.CrmConstants;
 import ru.protei.portal.core.model.util.sqlcondition.Condition;
 import ru.protei.portal.core.model.util.sqlcondition.Query;
-import ru.protei.portal.core.model.util.sqlcondition.SqlQueryBuilder;
 import ru.protei.winter.jdbc.JdbcQueryParameters;
 
 import java.util.Collection;
@@ -19,8 +18,11 @@ import java.util.List;
 import static ru.protei.portal.core.model.helper.CollectionUtils.isEmpty;
 import static ru.protei.portal.core.model.helper.DateRangeUtils.makeInterval;
 import static ru.protei.portal.core.model.util.sqlcondition.SqlQueryBuilder.condition;
+import static ru.protei.portal.core.model.util.sqlcondition.SqlQueryBuilder.query;
+
 
 public class CaseCommentTimeElapsedSumDAO_Impl extends PortalBaseJdbcDAO<CaseCommentTimeElapsedSum> implements CaseCommentTimeElapsedSumDAO {
+
     @Override
     public List<CaseCommentTimeElapsedSum> getListByQuery( CaseQuery caseCommentQuery ) {
         return getList( makeJdbcQueryParameters( caseCommentQuery ) );
@@ -31,7 +33,7 @@ public class CaseCommentTimeElapsedSumDAO_Impl extends PortalBaseJdbcDAO<CaseCom
     private JdbcQueryParameters makeJdbcQueryParameters( CaseQuery query ) {
         Interval interval = makeInterval(query.getCreatedRange());
 
-        Query sqlQuery = SqlQueryBuilder.query()
+        Query sqlQuery = query()
                 .where( "case_object.id" ).equal( query.getId() )
                 .and( "case_object.caseno" ).in( query.getCaseNumbers() )
                 .and( "case_object.product_id" ).in( query.getProductIds() )
@@ -41,10 +43,10 @@ public class CaseCommentTimeElapsedSumDAO_Impl extends PortalBaseJdbcDAO<CaseCom
                 .and( "case_comment.created" ).ge( interval.getFrom() )
                 .and( "case_comment.created" ).lt( interval.getTo() )
                 .and( "case_comment.time_elapsed" ).not().isNull( true )
-                .and( getConditionByTimeElapsedTypeIds(query.getTimeElapsedTypeIds()) )
+                .and( makeConditionByTimeElapsedTypeIds(query.getTimeElapsedTypeIds()) )
                 .and( "case_comment.author_id" ).in( query.getCommentAuthorIds() )
 
-                .and(getConditionByCaseTagsIds(query.getCaseTagsIds()))
+                .and( makeConditionWithSubqueryByCaseTagsIds(query.getCaseTagsIds()) )
 
                 .asQuery()
                 .offset( query.getOffset() )
@@ -57,7 +59,7 @@ public class CaseCommentTimeElapsedSumDAO_Impl extends PortalBaseJdbcDAO<CaseCom
         return sqlQuery.build();
     }
 
-    private Condition getConditionByTimeElapsedTypeIds(Collection<Integer> timeElapsedTypeIds) {
+    private Condition makeConditionByTimeElapsedTypeIds(Collection<Integer> timeElapsedTypeIds) {
         if (isEmpty(timeElapsedTypeIds)) {
             return condition();
         }
@@ -71,11 +73,14 @@ public class CaseCommentTimeElapsedSumDAO_Impl extends PortalBaseJdbcDAO<CaseCom
                 .or("case_comment.time_elapsed_type").isNull(true);
     }
 
-    private Condition getConditionByCaseTagsIds(Collection<Long> caseTagsIds) {
-        if (isEmpty(caseTagsIds)) {
+    private Condition makeConditionWithSubqueryByCaseTagsIds(Collection<Long> caseTagsIds) {
+        if (isEmpty(caseTagsIds) || caseTagsIds.contains(CrmConstants.CaseTag.NOT_SPECIFIED)) {
             return condition();
         }
 
-        return condition().or("tag_id").in(caseTagsIds);
+        Query subQuery = query().select("distinct case_id").from("case_object_tag")
+                .where("tag_id").in(caseTagsIds).asQuery();
+
+        return  condition().and("case_object.ID").in(subQuery);
     }
 }
