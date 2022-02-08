c45a3dee51d434ff29230daa17b5001d9502fd61
nunaev <nunaev@protei.ru>
2022-02-09 00:26:55 +0300

    PORTAL-2121 add loader to reports

---
+++ gwt/report/src/main/java/ru/protei/portal/ui/report/client/view/table/columns/LoaderColumn.java
@@ -0,0 +1,34 @@
+package ru.protei.portal.ui.report.client.view.table.columns;
+
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Element;
+import ru.protei.portal.core.model.dto.ReportDto;
+import ru.protei.portal.ui.common.client.columns.ClickColumn;
+import ru.protei.portal.ui.common.client.columns.StaticColumn;
+
+public class LoaderColumn<T> extends ClickColumn<T> {
+    @Override
+    protected void fillColumnHeader(Element columnHeader) {
+        columnHeader.setInnerText("");
+    }
+
+    @Override
+    protected void fillColumnValue(Element cell, T value) {
+        com.google.gwt.dom.client.Element parentDiv = DOM.createDiv();
+        parentDiv.setClassName("lds-facebook");
+
+        com.google.gwt.dom.client.Element firstChildDiv = DOM.createDiv();
+        parentDiv.appendChild(firstChildDiv);
+
+        com.google.gwt.dom.client.Element secondChildDiv = DOM.createDiv();
+        parentDiv.appendChild(secondChildDiv);
+
+        com.google.gwt.dom.client.Element thirdChildDiv = DOM.createDiv();
+        parentDiv.appendChild(thirdChildDiv);
+
+        com.google.gwt.dom.client.Element fourthChildDiv = DOM.createDiv();
+        parentDiv.appendChild(fourthChildDiv);
+
+        cell.appendChild(parentDiv);
+    }
+}
