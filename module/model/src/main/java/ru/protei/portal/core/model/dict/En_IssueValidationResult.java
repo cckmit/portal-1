7ec0a1e8ab0b2d1ac340a18ca6f7c7c8acc51ca5
porubov <porubov@protei.ru>
2022-02-16 12:46:11 +0300

    PORTAL-2046. after review

--- module/model/src/main/java/ru/protei/portal/core/model/dict/En_IssueValidationResult.java
+++ module/model/src/main/java/ru/protei/portal/core/model/dict/En_IssueValidationResult.java
@@ -2,62 +2,77 @@ package ru.protei.portal.core.model.dict;
 
 public enum En_IssueValidationResult {
     OK,
+
     /**
      * Case object cannot be null
      */
     NULL,
+
     /**
      * Name must be specified
      */
     NAME_EMPTY,
+
     /**
      * Type must be specified
      */
     TYPE_EMPTY,
+
     /**
      * Required creator id
      */
     CREATOR_EMPTY,
+
     /**
      * Importance level must be specified
      */
     IMPORTANCE_EMPTY,
+
     /**
      * Manager company must be specified
      */
     MANAGER_EMPTY,
+
     /**
      * Manager must belong to company
      */
     MANAGER_OTHER_COMPANY,
+
     /**
      * Manager must be specified with product
      */
     MANAGER_WITHOUT_PRODUCT,
+
     /**
      * Initiator company must be specified
      */
     INITIATOR_EMPTY,
+
     /**
      * State is not valid
      */
     STATUS_INVALID,
+
     /**
      * Importance level must belong to company
      */
     IMPORTANCE_OTHER_COMPANY,
+
     /**
      * Initiator must belong to company
      */
     INITIATOR_OTHER_COMPANY,
+
     /**
      * Platform must belong to initiator company
      */
     PLATFORM_OTHER_COMPANY,
+
     /**
      * Product is not valid
      */
     PRODUCT_INVALID,
+
     /**
      * Deadline has passed
      */
