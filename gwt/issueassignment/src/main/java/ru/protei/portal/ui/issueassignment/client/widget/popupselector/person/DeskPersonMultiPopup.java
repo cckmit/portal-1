341f6a1bc510cc93e65fdb599433f4fbbd0cd927
turik <turik@protei.ru>
2022-03-01 17:35:32 +0300

    Changed DeskPersonMultiPopup model

--- gwt/issueassignment/src/main/java/ru/protei/portal/ui/issueassignment/client/widget/popupselector/person/DeskPersonMultiPopup.java
+++ gwt/issueassignment/src/main/java/ru/protei/portal/ui/issueassignment/client/widget/popupselector/person/DeskPersonMultiPopup.java
@@ -3,11 +3,15 @@ package ru.protei.portal.ui.issueassignment.client.widget.popupselector.person;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.inject.Inject;
+import ru.protei.portal.core.model.dict.En_SortDir;
+import ru.protei.portal.core.model.dict.En_SortField;
+import ru.protei.portal.core.model.query.EmployeeQuery;
 import ru.protei.portal.core.model.util.CrmConstants;
 import ru.protei.portal.core.model.view.PersonShortView;
 import ru.protei.portal.ui.common.client.lang.Lang;
 import ru.protei.portal.ui.common.client.selector.SelectorPopup;
 import ru.protei.portal.ui.common.client.widget.composite.popper.PopperComposite;
+import ru.protei.portal.ui.common.client.widget.selector.person.EmployeeCustomModel;
 import ru.protei.portal.ui.common.client.widget.selector.person.EmployeeModel;
 import ru.protei.portal.ui.issueassignment.client.widget.popupselector.PopupMultiSelector;
 
@@ -18,9 +22,10 @@ import java.util.function.Consumer;
 public class DeskPersonMultiPopup extends PopupMultiSelector<PersonShortView> {
 
     @Inject
-    public void init(EmployeeModel model, Lang lang) {
+    public void init(EmployeeCustomModel model, Lang lang) {
         this.model = model;
         setAsyncModel(model);
+        model.setEmployeeQuery(new EmployeeQuery(null, false, null, En_SortField.person_full_name, En_SortDir.ASC));
         setFilter(personView -> !personView.isFired());
         setItemRenderer(PersonShortView::getDisplayName);
         setPageSize(CrmConstants.DEFAULT_SELECTOR_PAGE_SIZE);
@@ -51,5 +56,5 @@ public class DeskPersonMultiPopup extends PopupMultiSelector<PersonShortView> {
         model.clear();
     }
 
-    private EmployeeModel model;
+    private EmployeeCustomModel model;
 }
