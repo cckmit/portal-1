198c7ad4afbd5ae83fb94a4254ea34c755c9542c
buchmastov <buchmastov@protei.ru>
2022-05-25 15:15:12 +0300

    PORTAL-2184 - fix ajv-formats

--- webts/packages/common/common/src/js/infrastructure/json-validation/ajv.ts
+++ webts/packages/common/common/src/js/infrastructure/json-validation/ajv.ts
@@ -1,11 +1,13 @@
 import Ajv from "ajv"
+import addFormats from "ajv-formats"
 import { DEV } from "../../globals"
 
-export const ajv = new Ajv({
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-assignment
+export const ajv: Ajv = addFormats(new Ajv({
   validateSchema: DEV,
   allErrors: true,
   removeAdditional: true,
   code: {
     lines: true,
   },
-})
+}))
