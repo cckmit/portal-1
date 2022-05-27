303f0598954f1819f19c96e68bf403afa009b841
porubov <porubov@protei.ru>
2022-05-27 10:26:27 +0300

    PORTAL-2196. remove debug code

--- module/core/src/main/java/ru/protei/portal/core/controller/api/json/DeliverySpecificationApiController.java
+++ module/core/src/main/java/ru/protei/portal/core/controller/api/json/DeliverySpecificationApiController.java
@@ -13,20 +13,15 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import ru.protei.portal.api.struct.Result;
 import ru.protei.portal.core.controller.api.json.utils.JsonRequest;
 import ru.protei.portal.core.controller.api.json.utils.JsonResponse;
-import ru.protei.portal.core.model.dict.En_Privilege;
 import ru.protei.portal.core.model.dict.En_ResultStatus;
-import ru.protei.portal.core.model.dict.En_Scope;
 import ru.protei.portal.core.model.ent.AuthToken;
 import ru.protei.portal.core.model.ent.DeliverySpecification;
-import ru.protei.portal.core.model.ent.UserRole;
 import ru.protei.portal.core.model.query.DeliverySpecificationQuery;
 import ru.protei.portal.core.service.DeliverySpecificationService;
 import ru.protei.portal.core.service.session.SessionService;
 import ru.protei.winter.core.utils.beans.SearchResult;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 
 import static ru.protei.portal.api.struct.Result.error;
@@ -59,7 +54,7 @@ public class DeliverySpecificationApiController {
         log.info("API | getDeliverySpecifications(): Query={}", query);
 
         return new JsonResponse<>(query.getRequestId(),
-                getFakeAuthToken(request)
+                getAuthToken(request)
                         .flatMap(authToken -> service.getDeliverySpecifications(authToken, query.getData()))
         );
     }
@@ -74,7 +69,7 @@ public class DeliverySpecificationApiController {
         log.info("API | getDeliverySpecification(): id={}", id);
 
         return new JsonResponse<>(id.getRequestId(),
-                getFakeAuthToken(request)
+                getAuthToken(request)
                         .flatMap(authToken -> service.getDeliverySpecification(authToken, id.getData()))
         );
     }
@@ -89,7 +84,7 @@ public class DeliverySpecificationApiController {
         log.info("API | createDeliverySpecification(): deliverySpecification={}", deliverySpecification);
 
         return new JsonResponse<>(deliverySpecification.getRequestId(),
-                getFakeAuthToken(request)
+                getAuthToken(request)
                         .flatMap(authToken -> service.createDeliverySpecification(authToken, deliverySpecification.getData()))
         );
     }
@@ -104,7 +99,7 @@ public class DeliverySpecificationApiController {
         log.info("API | createDeliverySpecifications(): list={}", deliverySpecificationList);
 
         return new JsonResponse<>(deliverySpecificationList.getRequestId(),
-                getFakeAuthToken(request)
+                getAuthToken(request)
                         .flatMap(authToken -> service.createDeliverySpecifications(authToken, deliverySpecificationList.getData()))
         );
     }
@@ -116,45 +111,4 @@ public class DeliverySpecificationApiController {
         }
         return ok(authToken);
     }
-
-    private Result<AuthToken> getFakeAuthToken(HttpServletRequest request) {
-        AuthToken token = new AuthToken("test-session-id");
-        token.setIp("127.0.0.1");
-        token.setUserLoginId(15550L);
-        token.setPersonId(7777L);
-        token.setPersonDisplayShortName("DeliverySpecificationApiController");
-        token.setCompanyId(1L);
-        token.setCompanyAndChildIds(null);
-        token.setRoles(makeRoles());
-        return ok(token);
-    }
-
-    private HashSet<UserRole> makeRoles() {
-        UserRole role = new UserRole();
-        role.setPrivileges(new HashSet<>(Arrays.asList(PRIVILEGES)));
-        role.setScope(En_Scope.SYSTEM);
-        return new HashSet<>(Arrays.asList(role));
-    }
-
-    private static final En_Privilege[] PRIVILEGES = new En_Privilege[] {
-            En_Privilege.ISSUE_VIEW, En_Privilege.ISSUE_EDIT, En_Privilege.ISSUE_CREATE,
-            En_Privilege.PRODUCT_VIEW, En_Privilege.PRODUCT_EDIT, En_Privilege.PRODUCT_CREATE,
-            En_Privilege.COMPANY_VIEW, En_Privilege.COMPANY_EDIT, En_Privilege.COMPANY_CREATE,
-            En_Privilege.CONTACT_VIEW, En_Privilege.CONTACT_EDIT, En_Privilege.CONTACT_CREATE,
-            En_Privilege.EMPLOYEE_VIEW, En_Privilege.PROJECT_CREATE,
-            En_Privilege.SUBNET_VIEW, En_Privilege.SUBNET_CREATE,
-            En_Privilege.SUBNET_EDIT, En_Privilege.SUBNET_REMOVE,
-            En_Privilege.RESERVED_IP_VIEW, En_Privilege.RESERVED_IP_CREATE,
-            En_Privilege.RESERVED_IP_EDIT, En_Privilege.RESERVED_IP_REMOVE,
-            En_Privilege.PLAN_CREATE, En_Privilege.PLAN_EDIT, En_Privilege.PLAN_REMOVE, En_Privilege.PLAN_VIEW,
-            En_Privilege.PROJECT_CREATE, En_Privilege.PROJECT_EDIT, En_Privilege.PROJECT_REMOVE, En_Privilege.PROJECT_VIEW,
-            En_Privilege.SITE_FOLDER_CREATE, En_Privilege.SITE_FOLDER_REMOVE, En_Privilege.SITE_FOLDER_EDIT,
-            En_Privilege.ABSENCE_CREATE,
-            En_Privilege.DOCUMENT_CREATE, En_Privilege.DOCUMENT_REMOVE,
-            En_Privilege.DELIVERY_CREATE,
-            En_Privilege.CASE_STATES_VIEW,
-            En_Privilege.CONTRACT_VIEW,
-            En_Privilege.DELIVERY_VIEW,
-            En_Privilege.DELIVERY_SPECIFICATION_VIEW, En_Privilege.DELIVERY_SPECIFICATION_CREATE
-    };
 }
